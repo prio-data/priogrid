99044f1a4cb21fd17818f2a3e51c73322ac26647
Peder Landsverk <pglandsverk@gmail.com>
2019-07-10 14:36:25 +0200

    Rewrote make_gwcode
    The function is much fancier, only does heavy calculations when it needs
    to, et.c. Needs some polish, like merging the rasters at the end, but
    works like a charm!

--- R/make_gwcode.R
+++ R/make_gwcode.R
@@ -1,68 +1,109 @@
 
+mk_gwcode <- function(cshapes, partial = TRUE, ...){
 
+   unique_dates <- unique(c(cshapes$startdate, cshapes$enddate))
+   unique_dates <- sort(unique_dates)
 
-make_gwno <- function(cshp, date_override = NULL, detail = NULL, ncores = 1){
-   tictoc::tic('Doing the whole thing')
+   if(partial){
+      whichdates <- sample(c(TRUE,FALSE), size = length(unique_dates), 
+                           replace = TRUE, prob = c(0.1,0.9))
+      whichdates[1] <- TRUE
+      whichdates[length(whichdates)] <- TRUE
+      unique_dates <- unique_dates[whichdates]
+   }
 
-   blank_grid <- priogrid::prio_blank_grid()
-   full_landgrid <- priogrid::landgrid(cshp, blank = blank_grid) 
+   base <- prio_blank_grid()
 
-   #borders <- st_simplify(cshp,dTolerance = 1) %>%
-   #   st_boundary() %>%
-   #   st_buffer(1) %>%
-   #   st_simplify(dTolerance = 1)
-   #borderVelox <- velox(blank_grid)
-   #borderVelox$rasterize(borders)
+   out <- list()
 
-   getGwRaster <- function(date, cshp, raster, detail){
-      tictoc::tic(glue::glue('Doing {strftime(date,"%d %b %Y")}'))
-      crossection <- cshp[date %within% cshp$date_interval,]
-      baseVelox <- velox::velox(raster)
-      baseVelox$rasterize(crossection, field = 'GWCODE', band = 1, background = NA)
-      baseRaster <- baseVelox$as.RasterStack()[[1]]
+   tictoc::tic('Looping over dates')
+   for(specdate in unique_dates){
+      specdate <- as.Date(specdate,as.Date('1970-01-01'))
+      print(glue::glue('Doing {specdate}'))
 
-      if(!is.null(detail)){
-         tictoc::tic('Detailing')
-         fineVelox <- velox::velox(raster::disaggregate(raster, detail))
-         fineVelox$rasterize(crossection, field = 'GWCODE', band = 1, background = NA)
-         fineRaster <- fineVelox$as.RasterStack()[[1]]
-         fineRaster <- raster::aggregate(fineRaster, detail, fun = raster::modal)
-         diff <- baseRaster != fineRaster
-         print(glue::glue('Diff: {sum(values(diff), na.rm = TRUE)}'))
-         #baseRaster[diff] <- fineRaster
-         tictoc::toc()
+      countries <- which(cshapes$startdate == specdate | cshapes$enddate == specdate)
+      cshapes_cs <- cshapes[countries,]
+
+      tictoc::tic('Time spent')
+
+      res <- rasterize_worldtiles(cshapes_cs, base,
+                   rasterize_tile, ...)
+
+      if(length(res) > 1){
+         res <- do.call(merge,res)
+      } else {
+         res <- res[[1]]
       }
 
+      out[[strftime(specdate)]] <- res
+
       tictoc::toc()
-      stck <- raster::stack(baseRaster,fineRaster,diff)
-      names(stck) <- c('base','fine','diff')
-      stck
-
    }
 
-   cshp <- cshp %>%
-     dplyr::filter(GWCODE != -1) %>%
-     dplyr::mutate(
-       startdate = lubridate::ymd(paste(GWSYEAR, GWSMONTH, GWSDAY, sep = "-")),
-       enddate = lubridate::ymd(paste(GWEYEAR, GWEMONTH, GWEDAY, sep = "-")),
-       date_interval = lubridate::interval(startdate, enddate)
-     )
-
-   unique_dates <- unique(c(cshp$startdate, cshp$enddate))
-    
-   if(!is.null(date_override)){
-      print(glue::glue('Overriding date'))
-      unique_dates <- date_override
-   }
-
-   print(glue::glue('Applying to {length(unique_dates)} dates!'))
-
-   unique_dates <- sort(unique_dates)
-   res <- parallel::mclapply(unique_dates, getGwRaster,
-          cshp = cshp,
-          raster = full_landgrid,
-          mc.cores = ncores,
-          detail = detail)
    tictoc::toc()
-   return(res)
+   out
+}
+
+rasterize_worldtiles <- function(vectors, raster, fun, 
+                         vectorsOfInterest = NULL, 
+                         subdiv = 4, ncore = 1, ...){
+
+   #if(is.null(vectorsOfInterest)){
+   #   # Only do function for relevant vectors,
+   #   # unless nothing was specified.
+   #   vectorsOfInterest <- 1:nrow(vectors)
+   #   print(glue::glue('Using all {length(vectorsOfInterest)} vectors'))
+   #} else {
+   #   print(glue::glue('Using {length(vectorsOfInterest)} vectors'))
+   #}
+
+   tiles <- raster::raster(nrow = subdiv, ncol = subdiv * 2,
+                   ext = raster::extent(raster),
+                   crs = raster::crs(raster))
+
+   polytiles <- spex::polygonize(tiles)
+   print(glue::glue('Using {nrow(polytiles)} tiles'))
+
+   res <- parallel::mclapply(1:nrow(polytiles), mc.cores = ncore,
+            function(tilenumber){
+
+      intersections <- suppressMessages(sf::st_intersects(polytiles[tilenumber,], vectors))
+
+      if(any(intersections[[1]] %in% vectorsOfInterest)){
+         relevantvector <- vectors[intersections[[1]],]
+         relevantraster <- raster::crop(raster,polytiles[tilenumber,])
+         
+         tile_info <- list(intersections = intersections[[1]]) 
+
+         fun(relevantvector, relevantraster, tile_info,  ...)
+      } else {
+         NA
+      }
+   })
+
+   res[!is.na(res)]
+}
+
+rasterize_tile <- function(vec,ras, tile_info, detail = 2){
+   if(nrow(vec) > 0){
+      if(length(tile_info$intersections) > 1){
+         gigaras <- raster::disaggregate(ras,detail)
+         vx <- velox::velox(gigaras)
+
+         vx$rasterize(vec, 'GWCODE', 1, NA)
+         ras <- vx$as.RasterStack()[[1]]
+         raster::aggregate(ras, detail, fun = raster::modal)
+
+      } else {
+         vx <- velox::velox(ras)
+         vx$rasterize(vec, 'GWCODE', 1, NA)
+         vx$as.RasterStack()[[1]]
+      }
+
+   } else {
+      # Ocean tiles
+      raster::values(ras) <- NA
+      ras
+   }
+
 }
