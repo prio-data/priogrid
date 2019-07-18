c1f763a43e3a15a6edb4f2556b670ebbfbcf3f25
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2019-07-18 15:53:34 +0200

    Create fun_yearlyraster.R
    Function to create RasterStack consisting of one layer for each year

---
+++ R/fun_yearlyraster.R
@@ -0,0 +1,27 @@
+
+## Create RasterStack consisting of one RasterLayer for each year
+# NB! If we are working with RasterBrick instead, just change raster::stack to raster::brick
+
+# Prior to running, define variable and function for rasterization
+# variable <- "variable_name"
+# fun <- "function"
+
+
+yearly_stack <- function(data){
+years <- unique(data$year)
+emp <- list()
+
+for(i in 1:length(years)) {
+  y <- years[i]
+  if(sum(data$year == y) > 0)
+    emp[[i]] <- raster::rasterize(data[which(data$year == y),],
+                                  priogrid::prio_blank_grid(),
+                                  field = variable,
+                                  fun = fun)
+  }
+stack <- raster::stack(emp)
+names(stack) <- paste0(variable, years)
+return(stack)
+}
+
+
