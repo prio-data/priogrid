89b602431f2979755b93f0cb4f34149ae3bebfe9
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2019-07-22 10:01:37 +0200

    Update gen_cmr.R
    Added stacking

--- R/gen_cmr.R
+++ R/gen_cmr.R
@@ -3,19 +3,36 @@
 
 #' Generate cmr variables
 #' 
-#' @param cmr_data SEDAC Globa Subnational Prevalence of Child Malnutrition grid data (v.1)
+#' @param cmr_data SEDAC Global Subnational Prevalence of Child Malnutrition grid data (v.1)
 #' @param rast.fun Function to aggregate by ('mean', 'sd', 'min', 'max')
 
 
 gen_cmr <- function(cmr_data, rast.fun){
   malnut <- raster::raster(cmr_data)
   
-  # Value error in original data cf. PG2 sql script
+  # Value error in original data cf. PG2 .sql script
   malnut <- malnut/10
   
-  malnut <- raster::aggregate(malnut, fact = resolution_factor(malnut),
+  malnut <- raster::aggregate(malnut, fact = priogrid::resolution_factor(malnut),
                       fun = rast.fun)
   return(malnut)
+}
+
+
+## Used file 'Uwq/uwq.asc' from the hunger-grid zip folder
+
+
+# Stack of sub-variables
+gen_cmr_stack <- function(cmr_data){
+  cmr_mean <- gen_cmr(cmr_data, rast.fun = 'mean')
+  cmr_sd <- gen_cmr(cmr_data, rast.fun = 'sd')
+  cmr_min <- gen_cmr(cmr_data, rast.fun = 'min')
+  cmr_max <- gen_cmr(cmr_data, rast.fun = 'max')
+  
+  cmr <- raster::stack(c(cmr_mean, cmr_sd, cmr_min, cmr_max))
+  names(cmr) <- c("cmr_mean", "cmr_sd", "cmr_min", "cmr_max")
+  return(cmr)
+  
 }
 
 
