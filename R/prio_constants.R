a6a6a36cfcbce601d36785bc9c9f66b6c5482630
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2019-07-22 09:55:06 +0200

    Added resolution_factor

--- R/prio_constants.R
+++ R/prio_constants.R
@@ -7,3 +7,8 @@ prio_nrow <- function(){360}
 prio_ncol <- function(){720}
 
 prio_midatlantic <- -29
+
+
+resolution_factor <- function(x){
+  priogrid::prio_resolution()/res(x) # Calculates the factor to aggregate by to get 0.5 x 0.5 resolution
+}
