162af7223893640fb368e2116d7d84a09f294791
Vilde Bergstad Larsen <villar@prio.org>
2019-08-01 14:23:24 +0200

    Update gen_cmr.R
    Updated to include prio_aggregate_raster, returning rasterbrick consistent with prio-grid

--- R/gen_cmr.R
+++ R/gen_cmr.R
@@ -1,36 +1,24 @@
 
 # Child malnutrition ------------------------------------------------------
 
-#' Generate cmr variables
+#' Generate cmr variables RasterBrick.
 #' 
 #' @param cmr_data SEDAC Global Subnational Prevalence of Child Malnutrition grid data (v.1, uw.asc)
-#' @param rast.fun Function to aggregate by ('mean', 'sd', 'min', 'max')
 
 
-gen_cmr <- function(cmr_data, rast.fun){
+gen_cmr <- function(cmr_data){
   malnut <- raster::raster(cmr_data)
   
   # Value error in original data cf. PG2 .sql script
   malnut <- malnut/10
   
-  malnut <- raster::aggregate(malnut, fact = priogrid::resolution_factor(malnut),
-                      fun = rast.fun)
-  return(malnut)
-}
-
-
-
-
-
-# Brick of sub-variables
-gen_cmr_brick <- function(cmr_data){
-  cmr_mean <- gen_cmr(cmr_data, rast.fun = 'mean')
-  cmr_sd <- gen_cmr(cmr_data, rast.fun = 'sd')
-  cmr_min <- gen_cmr(cmr_data, rast.fun = 'min')
-  cmr_max <- gen_cmr(cmr_data, rast.fun = 'max')
+  cmr_mean <- priogrid::prio_aggregate_raster(malnut, fun = 'mean')
+  cmr_sd <- priogrid::prio_aggregate_raster(malnut, rast.fun = 'sd')
+  cmr_min <- priogrid::prio_aggregate_raster(malnut, rast.fun = 'min')
+  cmr_max <- priogrid::prio_aggregate_raster(malnut, rast.fun = 'max')
   
   cmr <- raster::brick(c(cmr_mean, cmr_sd, cmr_min, cmr_max))
   names(cmr) <- c("cmr_mean", "cmr_sd", "cmr_min", "cmr_max")
-  return(cmr)
   
+  return(malnut)
 }
