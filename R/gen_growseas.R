9bae20ede9f1bb38039565a04eaae77ce36bc0ec
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2019-08-27 10:46:19 +0200

    Fixed data.table error
    Fixed syntax resulting in error when using data.table within function

--- R/gen_growseas.R
+++ R/gen_growseas.R
@@ -25,10 +25,10 @@ prep_growseas <- function(mirca){
     dplyr::summarize(area = sum(area))
 
   data.table::setDT(m_small)
-  m_small <- m_small[, .(lat = lat, long = long, area = area, start = start, end = end, month = priogrid::select_months(start, end)),
-                     .(1:nrow(m_small))][, nrow := NULL][]
+  m_small <- m_small[, list(lat = lat, long = long, area = area, start = start, end = end, month = priogrid::select_months(start, end)),
+                     list(1:nrow(m_small))][, nrow := NULL][]
 
-  m_small <- m_small[, .(area = sum(area, na.rm = T)), by = c("lat", "long", "month")]
+  m_small <- m_small[, list(area = sum(area, na.rm = T)), by = c("lat", "long", "month")]
 
 
   m_small <- m_small %>%
