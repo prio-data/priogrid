108626feec0f3d57a26493a71e718c3ad71d117a
Jonas Vestby <jonves@prio.org>
2019-09-10 14:50:05 +0200

    Working on the script to build priogrid netcdf. pop_gpw and diamonds.

--- R/gen_diamonds.R
+++ R/gen_diamonds.R
@@ -8,30 +8,30 @@
 
 
 gen_diamprim_y <- function(diamond_data){
-  
+
   diamonds <- priogrid::prep_diamonds(diamond_data)
-  
+
   diamonds <- priogrid::yearly_dummy(data = diamonds, endyear = 2005) %>%
     dplyr::rename(diamprim_y = diamprim)
-  
+
   diamonds <- priogrid::yearly_brick(diamonds, variable = "diamprim_y", raster.fun = "first")
-    
+
 }
 
 
 #' Generate yearly secondary diamond presence dummy.
-#' 
+#'
 #' @param diamond_data DIADATA shapefile from the PRIO Diamond Resources dataset.
 
 gen_diamsec_y <- function(diamond_data){
-  
+
   diamonds <- priogrid::prep_diamonds(diamond_data)
-  
+
   diamonds <- priogrid::yearly_dummy(data = diamonds, endyear = 2005) %>%
     dplyr::rename(diamsec_y = diamsec)
-  
+
   diamonds <- priogrid::yearly_brick(diamonds, variable = "diamsec_y", raster.fun = "first")
-  
+
 }
 
 
@@ -46,18 +46,19 @@ gen_diamsec_y <- function(diamond_data){
 
 gen_diamonds_s <- function(diamond_data){
   diamonds <- priogrid::prep_diamonds(diamond_data)
-  
+
   diamonds <- priogrid::static_dummy(diamonds) %>%
     dplyr::rename(diamsec_s = diamsec, diamprim_s = diamprim)
-  
+
   # Rasterize
-  diamprim_s <- raster::rasterize(diamonds, priogrid::prio_blank_grid(), 
-                                  field = "diamprim_s", fun = "first")
-  
+  diamprim_s <- raster::rasterize(diamonds, priogrid::prio_blank_grid(),
+                                  field = "diamprim_s", fun = "count")
+
   diamsec_s <- raster::rasterize(diamonds, priogrid::prio_blank_grid(),
-                                 field = "diamsec_s", fun = "first")
-  
+                                 field = "diamsec_s", fun = "count")
+
   brick <- raster::brick(c(diamprim_s, diamsec_s))
+  names(brick) <- c("diamprim_s", "diamsec_s")
   return(brick)
 }
 
@@ -67,20 +68,20 @@ gen_diamonds_s <- function(diamond_data){
 # Diamonds data prep function ---------------------------------------------
 
 prep_diamonds <- function(diamond_data){
-  
+
   diamonds <- sf::st_read(diamond_data)
   diamonds <- sf::st_transform(diamonds, crs = priogrid::prio_crs())
-  
+
   diamonds$disc.year <- lubridate::year(diamonds$DISC)
   diamonds$PROD <- base::as.Date(diamonds$PROD, format = "%d/%m/%Y")
   diamonds$prod.year <- lubridate::year(diamonds$PROD)
-  
+
   diamonds <- diamonds %>%
     dplyr::select(gwno = COWCODE, id = PRIMKEY, disc.year, prod.year, DIAINFO, geometry) %>%
     dplyr::mutate(diamprim = ifelse(DIAINFO == "P", yes = 1, no = NA),
                   diamsec = ifelse(DIAINFO == "S", yes = 1, no = NA)) %>%
     dplyr::filter(diamprim == 1 | diamsec == 1)
-  
+
   diamonds
 }
 
--- R/raster_functions.R
+++ R/raster_functions.R
@@ -1,5 +1,5 @@
 # ================================
-# Functions for working with 
+# Functions for working with
 # rasters
 # ================================
 
@@ -10,32 +10,32 @@ prio_aggregate_raster <- function(x, fun){
    fact <- priogrid::resolution_factor(x)
    res <- raster::aggregate(x,fact=fact,fun=fun)
    raster::crs(res) <- priogrid::prio_crs()
-   
+
    pg <- priogrid::prio_blank_grid()
    raster::values(pg) <- NA
-   
+
    raster::origin(res) <- raster::origin(pg)
-   
+
    res <- raster::merge(res, pg, overlap = FALSE)
-   
+
    res
 }
 
 
 prio_raster <- function(x){
   # Returns raster with same extent and dimensions as PG, use when aggregation is not needed
-  
+
   raster::crs(x) <- priogrid::prio_crs()
-  
+
   pg <- priogrid::prio_blank_grid()
   raster::values(pg) <- NA
-  
+
   raster::origin(x) <- raster::origin(pg)
-  
+
   x <- raster::merge(x, pg, overlap = FALSE)
-  
+
   x
-  
+
 }
 
 
@@ -48,10 +48,10 @@ prio_raster <- function(x){
     lat <- NA
     res <- NA
   } else{
-    lon <- ncvar_get(nc, lon, verbose = F)
-    lat <- ncvar_get(nc, lat, verbose = F)
-    lonres <- (lon[2]-lon[1])/2
-    latres <- (lat[2]-lat[1])/2
+    longitudes <- ncvar_get(nc, lon, verbose = F)
+    latitudes <- ncvar_get(nc, lat, verbose = F)
+    lonres <- 360 / length(longitudes)
+    latres <- 180 / length(latitudes)
 
   }
 
@@ -62,8 +62,8 @@ prio_raster <- function(x){
   nc_array[nc_array == fillvarname$value] <- NA
   nc_close(nc)
 
-  return(list("data" = nc_array, 
-              "lon" = lon, "lat" = lat, 
+  return(list("data" = nc_array,
+              "lon" = longitudes, "lat" = latitudes,
               "lonres" = lonres, "latres" = latres))
 }
 
@@ -77,16 +77,16 @@ make_raster <- function(nclist, transpose=FALSE, crs=NULL){
      data <- nclist$data
   }
 
-  raster(data, 
-      xmn =  min(nclist$lon)-nclist$lonres, 
+  raster(data,
+      xmn =  min(nclist$lon)-nclist$lonres,
       xmx = max(nclist$lon)+nclist$lonres,
-      ymn = min(nclist$lat)-nclist$latres, 
+      ymn = min(nclist$lat)-nclist$latres,
       ymx = max(nclist$lat)+nclist$latres,
       crs=crs)
 }
 
 #' Raster points
-#' 
+#'
 #' Simple function that returns a raster as a sf set of points
 raster_points <- function(raster, na.rm = TRUE){
 
@@ -102,7 +102,7 @@ raster_points <- function(raster, na.rm = TRUE){
       })
 
    tibble::tibble(val = raster_values) %>%
-     st_sf(geometry = pts) 
+     st_sf(geometry = pts)
 }
 
 #' stepwiseAggregate
@@ -115,8 +115,8 @@ raster_points <- function(raster, na.rm = TRUE){
 #' Use the function "hugeAggregate" instead of this one,
 #' since this one will output a raster with an arbitrary
 #' instead of a target resolution.
-#' 
-#' @arg data a rasterlayer 
+#'
+#' @arg data a rasterlayer
 #' @arg minres won't aggregate past this mininmum res
 #' @arg fun what function to use
 #' @arg firstpass aggregate first round with min, saving time
@@ -136,7 +136,7 @@ stepwiseAggregate <- function(data, minres, fun = priogrid::quickmode, firstpass
       resultant <- length(data) / 4
    }
 }
-   
+
 
 
 #' Create RasterBrick consisting of one RasterLayer for each year
@@ -148,7 +148,7 @@ stepwiseAggregate <- function(data, minres, fun = priogrid::quickmode, firstpass
 yearly_brick <- function(data, variable, raster.fun){
   years <- unique(data$year)
   emp <- list()
-  
+
   for(i in 1:length(years)) {
     y <- years[i]
     if(sum(data$year == y) > 0)
@@ -160,29 +160,29 @@ yearly_brick <- function(data, variable, raster.fun){
   brick <- raster::brick(emp)
   names(brick) <- paste0(variable,"_", years)
   return(brick)
-} ## NOTE: This is fairly quick on smaller point data, but takes a lot of time on larger data. 
+} ## NOTE: This is fairly quick on smaller point data, but takes a lot of time on larger data.
 
 
 
 
-#' Rotate extent of raster 
-#' 
+#' Rotate extent of raster
+#'
 #' Function to change raster latitudes from (0, 360) to (-180, 180),
-#' the former is commonly used in climate data. 
-#' 
+#' the former is commonly used in climate data.
+#'
 #' @param raster RasterLayer or RasterBrick.
 
 rotateExtent <- function(raster){
   ext1 <- raster::extent(0, 180, -90, 90)
   ext2 <- raster::extent(180, 360, -90, 90)
-  
+
   rightCrop <- raster::crop(raster, ext1)
-  
+
   leftCrop <- raster::crop(raster, ext2)
-  
+
   full <- raster::merge(rightCrop, leftCrop)
   full <- raster::rotate(full)
-  
+
   full
 }
 
