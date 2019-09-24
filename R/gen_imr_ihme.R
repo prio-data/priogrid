f5a84a17857a41e6f051ae7f8425f195ecd4f35c
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2019-09-24 14:54:49 +0200

    Added function to create brick of all subvariables

--- R/gen_imr_ihme.R
+++ R/gen_imr_ihme.R
@@ -7,12 +7,23 @@
 #' @param ihme_ IHME mean geoTIFF files for years 2000, 2005, 2010 and 2015
 
 
+gen_imr_ihme <- function(ihme_00, ihme_05, ihme_10, ihme_15){
+  mean <- gen_imr_ihme_mean(ihme_00, ihme_05, ihme_10, ihme_15)
+  sd <- gen_imr_ihme_sd(ihme_00, ihme_05, ihme_10, ihme_15)
+  min <- gen_imr_ihme_min(ihme_00, ihme_05, ihme_10, ihme_15)
+  max <- gen_imr_ihme_max(ihme_00, ihme_05, ihme_10, ihme_15)
+
+  imr_ihme <- raster::brick(mean, sd, min, max)
+  imr_ihme
+}
+
+
 gen_imr_ihme_mean <- function(ihme_00, ihme_05, ihme_10, ihme_15){
   brick <- prep_imr_imhe(ihme_00, ihme_05, ihme_10, ihme_15)
 
   agg <- priogrid::prio_aggregate_raster(brick, fun = mean)
   years <- c(2000, 2005, 2010, 2015)
-  names(agg) <- paste0("imr_ihme_mean", years)
+  names(agg) <- paste0("imr_ihme_mean_", years)
 
   agg
 }
@@ -22,7 +33,7 @@ gen_imr_ihme_sd <- function(ihme_00, ihme_05, ihme_10, ihme_15){
 
   agg <- priogrid::prio_aggregate_raster(brick, fun = sd)
   years <- c(2000, 2005, 2010, 2015)
-  names(agg) <- paste0("imr_ihme_sd", years)
+  names(agg) <- paste0("imr_ihme_sd_", years)
 
   agg
 }
@@ -33,7 +44,7 @@ gen_imr_ihme_max <- function(ihme_00, ihme_05, ihme_10, ihme_15){
 
   agg <- priogrid::prio_aggregate_raster(brick, fun = max)
   years <- c(2000, 2005, 2010, 2015)
-  names(agg) <- paste0("imr_ihme_min", years)
+  names(agg) <- paste0("imr_ihme_min_", years)
 
   agg
 }
@@ -43,7 +54,7 @@ gen_imr_ihme_min <- function(ihme_00, ihme_05, ihme_10, ihme_15){
 
   agg <- priogrid::prio_aggregate_raster(brick, fun = min)
   years <- c(2000, 2005, 2010, 2015)
-  names(agg) <- paste0("imr_ihme_min", years)
+  names(agg) <- paste0("imr_ihme_min_", years)
 
   agg
 }
