3c16f6d80be01cf726fcce447d1942ef36b340d1
Peder Landsverk <pglandsverk@gmail.com>
2019-09-25 15:19:00 +0200

    Fixed and added three more variables to the config
    All three work afaik. Now only two remain!

--- R/gen_pop_gpw.R
+++ R/gen_pop_gpw.R
@@ -1,3 +1,36 @@
+
+#' gen_pop_gpw_sum 
+#' 
+#' @param path Path to gpw_nc NetCDF file. 
+#' @export
+gen_pop_gpw_sum <- function(path){
+   gen_pop_gpw(path,sum)
+}
+
+#' gen_pop_gpw_sd 
+#' 
+#' @param path Path to gpw_nc NetCDF file. 
+#' @export
+gen_pop_gpw_sd <- function(path){
+   gen_pop_gpw(path,sd)
+}
+
+#' gen_pop_gpw_min 
+#' 
+#' @param path Path to gpw_nc NetCDF file. 
+#' @export
+gen_pop_gpw_min <- function(path){
+   gen_pop_gpw(path,min)
+}
+
+#' gen_pop_gpw_max 
+#' 
+#' @param path Path to gpw_nc NetCDF file. 
+#' @export
+gen_pop_gpw_max <- function(path){
+   gen_pop_gpw(path,max)
+}
+
 ## Function to generate pop_gpw_sum, _sd, _min and _max
 ## Returns RasterBrick for the selected variable for years 2000, 2005, 2010 and 2015.
 
@@ -5,33 +38,16 @@
 #' Generate pop_gpw_ variables.
 #'
 #' @param gpw_nc Gridded Population of the World v.4 NetCDF file.
-#' @param rast.fun Function to aggregate by ('sum', 'sd', 'min', 'max')
+#' @param fun Function to aggregate by ('sum', 'sd', 'min', 'max')
 
-
-gen_pop_gpw <- function(gpw_nc, rast.fun){
+gen_pop_gpw <- function(gpw_nc, fun){
   gpw <- raster::brick(gpw_nc)
 
   gpw <- gpw[[1:5]] # 1:5 to also included estimate for 2020
   years <- c(2000, 2005, 2010, 2015, 2020)
 
-  gpw <- priogrid::prio_aggregate_raster(gpw, fun = rast.fun)
+  gpw <- priogrid::prio_aggregate_raster(gpw, fun = fun)
 
-  names(gpw) <- paste0("pop_gpw_", rast.fun, "_", years)
+  names(gpw) <- paste0("pop_gpw_", as.character(quote(rast.fun)), "_", years)
   return(gpw)
 }
-
-
-# Function to combine all subvariables in one stack - can include this in above function
-
-gen_pop_gpw_c <- function(gpw_nc){
-  sum <- gen_pop_gpw(gpw_nc, rast.fun = 'sum')
-  sd <- gen_pop_gpw(gpw_nc, rast.fun = 'sd')
-  min <- gen_pop_gpw(gpw_nc, rast.fun = 'min')
-  max <- gen_pop_gpw(gpw_nc, rast.fun = 'max')
-
-  stack <- raster::stack(sum, sd, min, max)
-  return(stack)
-
-}
-
-
