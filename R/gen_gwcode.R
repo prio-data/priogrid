8b9653d01a276c02d73ee3eaf846adbbd9db8d91
Jonas Vestby <jonves@prio.org>
2019-11-11 09:48:03 +0100

    Extracted the calculation of areas changing borders from month to month into separate function. gen_dcoast calculates distance to lakes and rivers.

--- R/gen_gwcode.R
+++ R/gen_gwcode.R
@@ -90,6 +90,78 @@ gen_landarea <- function(fname, output_folder, quiet = TRUE){
    return(landarea)
 }
 
+#' gen_changed_areas
+#'
+#' Takes the weidmann cshapes data set and returns a
+#' list of sf dataframes that corresponds to the areas where borders changed from one month to the next.
+#'
+#' @param fname File path to Weidmann cshapes data
+#' @param numCores Number of cores to use in calculation. Windows-users can only use 1. Using parallel::mclapply.
+#' @param quiet Whether or not sf::st_ functions should print warnings.
+gen_changed_areas <- function(fname, numCores = 1, quiet = TRUE){
+   compare_crossection <- function(crossection_date, cshp){
+      message(crossection_date)
+      if(crossection_date - lubridate::month(1) < min(cshp$startdate)){
+         changed_areas <- cshp[crossection_date %within% cshp$date_interval,]
+         changed_areas$crossection_date <- crossection_date
+      } else {
+         past_crossection <- cshp[(crossection_date - lubridate::month(1)) %within% cshp$date_interval,]
+         cshp_crossection <- cshp[crossection_date %within% cshp$date_interval,]
+         new_changes <- lengths(sf::st_equals_exact(cshp_crossection, past_crossection, par = 0)) == 0
+
+         if(any(new_changes)){
+            cshp_crossection$changes <- new_changes
+            past_crossection$changes <- lengths(sf::st_equals(past_crossection, cshp_crossection)) == 0
+
+            cshp_crossection <- dplyr::filter(cshp_crossection, changes)
+            past_crossection <- dplyr::filter(past_crossection, changes)
+
+            changed_areas <- rbind(cshp_crossection, past_crossection)
+            # Combine and buffer to make sure area-calculations are done again for bordering cells. st_union to check validity.
+            changed_areas <- sf::st_union(sf::st_buffer(sf::st_combine(changed_areas), 1))
+            changed_areas <- sf::st_sf(sf::st_union(sf::st_buffer(sf::st_combine(changed_areas), 1)))
+            changed_areas$crossection_date <- crossection_date
+
+         } else{
+            changed_areas <- NULL
+         }
+      }
+      return(changed_areas)
+   }
+
+   message("Loading cshapes")
+   cshp <- sf::st_read(fname, quiet = quiet)
+
+   # Setting day to first in month to ensure all changes are included.
+   cshp <- cshp %>%
+      dplyr::filter(GWCODE != -1) %>%
+      dplyr::mutate(
+         startdate = lubridate::ymd(paste(GWSYEAR, GWSMONTH, GWSDAY, sep = "-")),
+         enddate = lubridate::ymd(paste(GWEYEAR, GWEMONTH, GWEDAY, sep = "-"))) %>%
+      dplyr::mutate(
+         same_month = year(startdate) == year(enddate) & month(startdate) == month(enddate)
+      ) %>%
+      dplyr::filter(same_month == F & startdate != "1991-12-16") %>% # Hack for now. Multiple changes within each month.
+      dplyr::mutate(GWSDAY = 1,
+                    GWEDAY = 1) %>%
+      dplyr::mutate(
+         startdate = lubridate::ymd(paste(GWSYEAR, GWSMONTH, GWSDAY, sep = "-")),
+         enddate = lubridate::ymd(paste(GWEYEAR, GWEMONTH, GWEDAY, sep = "-"))) %>%
+      dplyr::mutate(
+         enddate = enddate - lubridate::days(1) # End up until, but not including
+      ) %>%
+      dplyr::mutate(
+         date_interval = lubridate::interval(startdate, enddate)
+      )
+
+   all_months <- seq(min(cshp$startdate), max(cshp$enddate), by = "1 month")
+   message("Find all areas and dates where there have been changes since last month.")
+   unique_crossections <- parallel::mclapply(all_months, compare_crossection, cshp, mc.cores = numCores)
+   unique_crossections[sapply(unique_crossections, is.null)] <- NULL
+
+   return(unique_crossections)
+}
+
 #' gen_gwcode_month
 #'
 #' Takes the weidmann cshapes data set and returns a
@@ -121,34 +193,12 @@ gen_gwcode_month <- function(fname, numCores = 1, quiet = quiet, output_folder =
       return(gwcode)
    }
 
-
-   compare_crossection <- function(crossection_date, cshp){
-      message(crossection_date)
-      if(crossection_date - lubridate::month(1) < min(cshp$startdate)){
-         changed_areas <- cshp[crossection_date %within% cshp$date_interval,]
-         changed_areas$crossection_date <- crossection_date
-      } else {
-         past_crossection <- cshp[(crossection_date - lubridate::month(1)) %within% cshp$date_interval,]
-         cshp_crossection <- cshp[crossection_date %within% cshp$date_interval,]
-         new_changes <- lengths(sf::st_equals_exact(cshp_crossection, past_crossection, par = 0)) == 0
-
-         if(any(new_changes)){
-            cshp_crossection$changes <- new_changes
-            past_crossection$changes <- lengths(sf::st_equals(past_crossection, cshp_crossection)) == 0
-
-            cshp_crossection <- dplyr::filter(cshp_crossection, changes)
-            past_crossection <- dplyr::filter(past_crossection, changes)
-
-            changed_areas <- rbind(cshp_crossection, past_crossection)
-            # Combine and buffer to make sure area-calculations are done again for bordering cells. st_union to check validity.
-            changed_areas <- sf::st_as_sf(sf::st_union(sf::st_buffer(sf::st_combine(changed_areas), 1)))
-            changed_areas$crossection_date <- crossection_date
-
-         } else{
-            changed_areas <- NULL
-         }
-      }
-      return(changed_areas)
+   message("Get the countries where the border changed from one month to the next.")
+   changed_areas_file <- paste0(output_folder, "changed_areas.rds")
+   if(!is.null(output_folder) &  file.exists(changed_areas_file)){
+      changed_areas <- readRDS(changed_areas_file)
+   } else{
+      break(paste(changed_areas_file, "does not exist. Please calculate changed_areas first."))
    }
 
    message("Get the set of grid cells that intersect with land from file.")
@@ -185,16 +235,11 @@ gen_gwcode_month <- function(fname, numCores = 1, quiet = quiet, output_folder =
          date_interval = lubridate::interval(startdate, enddate)
       )
 
-   all_months <- seq(min(cshp$startdate), max(cshp$enddate), by = "1 month")
-   message("Find all areas and dates where there have been changes since last month.")
-   unique_crossections <- parallel::mclapply(all_months, compare_crossection, cshp, mc.cores = numCores)
-   unique_crossections[sapply(unique_crossections, is.null)] <- NULL
-
    pg <- priogrid::prio_blank_grid()
 
    # ca 22 minutter på 1 kjerne
    message("Calculate gwcode-ownership for each cell, for each month where ownership changes somewhere in the world.")
-   gwcodes <- parallel::mclapply(unique_crossections, calc_crossection, mc.cores = numCores)
+   gwcodes <- parallel::mclapply(changed_areas, calc_crossection, mc.cores = numCores)
 
    # Update classification scheme iteratively. Gwcodes are only the pg-ids that have changed since last change.
    rasters <- list()
@@ -224,7 +269,7 @@ get_closest_distance <- function(points, features){
 }
 
 
-gen_dcoast <- function(output_folder, quiet = TRUE){
+gen_dcoast <- function(fname, output_folder, quiet = TRUE){
 
    message("Get the set of grid cells that intersect with land from file.")
    pgland_file <- paste0(output_folder, "pgland.rds")
@@ -239,8 +284,10 @@ gen_dcoast <- function(output_folder, quiet = TRUE){
    pgland <- sf::st_as_sf(pgland, coords = c("lon", "lat"))
    sf::st_crs(pgland) <- sf::st_crs(4326)
 
-   # this should be swapped with cshapes. we miss several islands here.
-   coastline <- rnaturalearth::ne_coastline(returnclass = "sf")
+
+   # We use GSHHG, with the "low" resolution (GSHHS_l_L1.shp)
+   coastline <- sf::st_read(fname, quiet = quiet)
+   coastline <- sf::st_boundary(coastline)
 
 
    pgland$dcoast <- priogrid::get_closest_distance(pgland, coastline)
@@ -249,6 +296,50 @@ gen_dcoast <- function(output_folder, quiet = TRUE){
 
    return(dcoast)
 }
+
+gen_driver(fnames, output_folder, quiet = TRUE){
+   river1 <- fnames[1]
+   river2 <- fnames[2]
+   river3 <- fnames[3]
+   lakes <- fnames[4]
+
+
+   #river1 <- "C:/Users/jonves/data/priogrid_data/gshhg/data/WDBII_shp/l/WDBII_river_l_L01.shp"
+   #river2 <- "C:/Users/jonves/data/priogrid_data/gshhg/data/WDBII_shp/l/WDBII_river_l_L02.shp"
+   #river3 <- "C:/Users/jonves/data/priogrid_data/gshhg/data/WDBII_shp/l/WDBII_river_l_L03.shp"
+   #lakes <- "C:/Users/jonves/data/priogrid_data/gshhg/data/GSHHS_shp/l/GSHHS_l_L2.shp"
+
+   river1 <- sf::st_read(river1, quiet = T)
+   river2 <- sf::st_read(river2, quiet = T)
+   river3 <- sf::st_read(river3, quiet = T)
+   lakes <- sf::st_read(lakes, quiet = T)
+   lakes <- dplyr::filter(lakes, area >= 1000)
+
+   lakes_and_rivers <- rbind(dplyr::select(river1, geometry),
+                             dplyr::select(river2, geometry),
+                             dplyr::select(river3, geometry),
+                             dplyr::select(lakes, geometry))
+
+   message("Get the set of grid cells that intersect with land from file.")
+   pgland_file <- paste0(output_folder, "pgland.rds")
+   if(!is.null(output_folder) &  file.exists(pgland_file)){
+      pgland <- readRDS(pgland_file)
+   } else{
+      return(paste(pgland_file, "does not exist. Please calculate pgland first."))
+   }
+
+   pgland <- raster::rasterToPoints(pgland)
+   pgland <- dplyr::tibble("gid" = pgland[,3], "lon" = pgland[,1], "lat" = pgland[,2])
+   pgland <- sf::st_as_sf(pgland, coords = c("lon", "lat"))
+   sf::st_crs(pgland) <- sf::st_crs(4326)
+   pgland$driver <- priogrid::get_closest_distance(pgland, lakes_and_rivers)
+
+   pg <- priogrid::prio_blank_grid()
+   driver <- raster::rasterize(pgland, pg, field = "driver")
+
+   return(driver)
+}
+
 
 gen_bdists <- function(fname, output_folder, numCores = 1, quiet = TRUE){
 
