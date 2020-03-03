d89524fcdaa0a8d2ca28827b1909e3878f4e369a
Peder <pglandsverk@gmail.com>
2020-03-03 15:04:21 +0100

    Added integrity checking, more control
    Also created a mockup of the master build function. It will:
    
    Get list of variables, check integrity, and figure out what can and
    should be done.
    
    Run wrapped instances of each variable function, which returns a
    dataframe which is subsequently saved.

---
+++ README.md
@@ -0,0 +1,13 @@
+# PRIO-GRID
+
+This is an R-package with code that can be used to reproduce PRIO-GRID v3. 
+It is possible to reproduce everything from single variables to the entire dataset.
+
+## Requirements:
+
+System requirements:
+```
+sudo apt install libgdal-dev
+sudo apt install libnetcdf-dev 
+```
+
--- tests/testthat/test_control.R
+++ tests/testthat/test_control.R
@@ -5,8 +5,8 @@ test_that("test auxillary control functions",{
    expect_error(lookup("foo","myVar"),"Lookup type must be one of")
    expect_error(lookup("variable","foo"),"specification in")
 
-   expect_equal(getInPath("in","foo"),"in/foo/data")
-   expect_equal(getOutPath("out","foo",".rds"),"out/foo.rds")
-   expect_equal(getOutPath("out","foo","csv"),"out/foo.csv")
+   expect_equal(get_in_path("in","foo"),"in/foo/data")
+   expect_equal(get_out_path("out","foo",".rds"),"out/foo.rds")
+   expect_equal(get_out_path("out","foo","csv"),"out/foo.csv")
 
 })
