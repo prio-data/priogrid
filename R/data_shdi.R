f6ad2d3979a72c6cf9ec42c182515edfbc82be67
Jonas Vestby <jonves@prio.org>
2020-03-03 11:05:23 +0100

    shdi oppdatert

--- R/data_shdi.R
+++ R/data_shdi.R
@@ -5,13 +5,13 @@
 #'
 #' @param path path to SHDI data.
 #' @param variable one of c("msch", "esch", "lifexp", "lgnic", "shdi")
-gen_shdi <- function(path, variable = "shdi"){
-  shdi <- read.csv(file.path(path,"SHDI-Complete SD1.csv"))
-  geom <- sf::read_sf(file.path(path,"GDL-SHDI-SHP-2.shp"))
+gen_shdi <- function(input_folder, variable = "shdi"){
+  shdi <- read.csv(file.path(input_folder, "SHDI-Complete SD1.csv"))
+  geom <- sf::st_read(file.path(input_folder, "GDL-SHDI-SHP-2.shp"))
 
   shdi <- shdi %>%
     dplyr::filter(level == "Subnat") %>%
-    dplyr::select(year, GDLCODE, esch)
+    dplyr::select(year, GDLCODE, !!variable)
 
   geom <- geom %>%
     dplyr::select(GDLCode, geometry)
@@ -20,10 +20,11 @@ gen_shdi <- function(path, variable = "shdi"){
   shdi.full <- merge(geom, shdi, by.x = "GDLCode", by.y = "GDLCODE")
   shdiyr <- priogrid::yearly_brick(shdi.full, variable = variable, raster.fun = 'mean')
 
+  return(shdiyr)
 }
 
-gen_msch <- function(path){ gen_shdi(path, variable == "msch") }
-gen_esch <- function(path){ gen_shdi(path, variable == "esch") }
-gen_lifexp <- function(path){ gen_shdi(path, variable == "lifexp") }
-gen_lgnic <- function(path){ gen_shdi(path, variable == "lgnic") }
+gen_msch <- function(input_folder){ gen_shdi(input_folder, variable == "msch") }
+gen_esch <- function(input_folder){ gen_shdi(input_folder, variable == "esch") }
+gen_lifexp <- function(input_folder){ gen_shdi(input_folder, variable == "lifexp") }
+gen_lgnic <- function(input_folder){ gen_shdi(input_folder, variable == "lgnic") }
 
