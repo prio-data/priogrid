8d2820a77dd1ecdecd2c27fb7a0f346c5ba1251a
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2020-03-10 16:07:55 +0100

    Finished gen_excluded

--- R/data_epr.R
+++ R/data_epr.R
@@ -1,53 +1,47 @@
-
-
-# Yearly ------------------------------------------------------------------
-
-#### Note: Takes about 15 mins to run due to for loop in rasterize fun.
-#### TODO Check that grid cells identifying groups belong to the same country as the group (gwcode).
-
-
-#' Generate excluded variable
+#' @title excluded
 #'
-#' Counts the number of excluded groups of politically relevant ethnic groups
-#' settled in a given grid cell in a given year.
+#' @description Generate variable that counts the number of excluded and politically relevant
+#' ethnic groups that are settled in a given grid cell in a given year.
 #'
-#' @param geoepr_data GeoEPR 2018 shapefile.
-#' @param epr_data EPR Core 2018 .csv data.
+#' @param input_folder Path to [pg-folder].
+#'
+#' @export
+gen_excluded <- function(input_folder){
+   files <- file.path(input_folder, "geoepr", "data", c("GeoEPR.shp","EPR-2018.1.1.csv"))
 
-gen_geoepr <- function(path){
-   files <- file.path(path,c("GeoEPR.shp","EPR-2018.1.1.csv"))
-
-   geoepr <- sf::st_read(files[1], stringsAsFactors = FALSE, quiet = TRUE)
+   geoepr <- sf::read_sf(files[1])
    geoepr <- sf::st_transform(geoepr, crs = priogrid::prio_crs())
- 
+
    geoepr <- geoepr %>%
       dplyr::select(gwno = gwid, from, to, group, grptype = type, groupid, geometry) %>%
       dplyr::mutate(from = as.numeric(from), to = as.numeric(to),
-                    year = priogrid::prio_year(from, to)) %>%
+                    year = purrr::map2(from, to, `:`)) %>%
       tidyr::unnest(year)
- 
+
    # Join with EPR to obtain group status
    epr <- read.csv(files[2])
- 
+
    epr <- epr %>%
       dplyr::select(gwno = gwid, groupid, status, from, to) %>%
       dplyr::mutate(excluded = ifelse(status == "POWERLESS" | status == "DISCRIMINATED",
                                      yes = 1, no = 0),
-                   year = priogrid::prio_year(from, to)) %>%
+                   year = purrr::map2(from, to, `:`)) %>%
       tidyr::unnest(year) %>%
       dplyr::select(gwno, groupid, year, excluded)
- 
+
    geoepr_epr <- base::merge(geoepr, epr,
                              by.x = c("gwno", "groupid", "year"),
                              by.y = c("gwno", "groupid", "year"),
                              all.x = TRUE)
- 
+
    geoepr_epr <- geoepr_epr[!sf::st_is_empty(geoepr_epr$geometry),]
 
-   brick <- priogrid::yearly_brick(geoepr_epr, 
-                                   variable = "excluded", 
-                                   raster.fun = "count")
+   epr_pg <- priogrid::panel_to_pg(geoepr_epr,
+                                timevar = "year",
+                                variable = "excluded",
+                                need_aggregation = TRUE,
+                                fun = "count")
 
-   return(brick)
+   return(epr_pg)
 }
 
