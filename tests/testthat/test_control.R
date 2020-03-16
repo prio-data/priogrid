c3991830424b06273b1f175e5eed197e3a03aa12
Peder <pglandsverk@gmail.com>
2020-03-16 10:55:40 +0100

    fixed lookup test

--- tests/testthat/test_control.R
+++ tests/testthat/test_control.R
@@ -1,9 +1,9 @@
 
 test_that("test auxillary control functions",{
 
-   expect_error(lookup("variable","gcp_mer"),NA)
-   expect_error(lookup("foo","myVar"),"Lookup type must be one of")
-   expect_error(lookup("variable","foo"),"specification in")
+   expect_error(specification_lookup("variable","gcp_mer"),NA)
+   expect_error(specification_lookup("foo","myVar"),"Lookup type must be one of")
+   expect_error(specification_lookup("variable","foo"),"specification in")
 
    expect_equal(get_in_path("in","foo"),"in/foo/data")
    expect_equal(get_out_path("out","foo",".rds"),"out/foo.rds")
