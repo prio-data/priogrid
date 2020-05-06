9b3d5eb5ab5eda540f4c76122ef8304eacf5bc99
Jonas Vestby <jonves@prio.org>
2020-05-06 21:55:16 +0200

    Filter makes the function run slightly faster. Result is identical.

--- R/data_cshp.R
+++ R/data_cshp.R
@@ -140,7 +140,7 @@ gen_pgland <- function(input_folder){
    cshp <- sf::read_sf(file.path(input_folder, "cshapes", "data", "cshapes.shp"))
 
    cshp <- cshp %>%
-      dplyr::filter(GWCODE != -1)
+      dplyr::filter(GWCODE != -1, GWEYEAR == 2016)
 
    pg <- priogrid::prio_blank_grid()
    pg_poly <- pg %>% spex::polygonize()
