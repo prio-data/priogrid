003d2c5db452c34c01e8e75bfeef973e4d589039
Jonas Vestby <jonves@prio.org>
2020-06-29 12:05:29 +0200

    6 decimals works?

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -20,10 +20,10 @@ raster_to_tibble <- function(rast, add_pg_index = FALSE){
     pg_df <- dplyr::as_tibble(pg_df)
 
     # merging floating point columns. need to round
-    df$x <- round(df$x, 10)
-    df$y <- round(df$y, 10)
-    pg_df$x <- round(pg_df$x, 10)
-    pg_df$y <- round(pg_df$y, 10)
+    df$x <- round(df$x, 6)
+    df$y <- round(df$y, 6)
+    pg_df$x <- round(pg_df$x, 6)
+    pg_df$y <- round(pg_df$y, 6)
 
     df <- dplyr::left_join(df, pg_df, by = c("x", "y"))
 
