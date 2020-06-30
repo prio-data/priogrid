12de8359b3d85723edebc4d3a91c6e79ca059a33
Jonas Vestby <jonves@prio.org>
2020-06-30 14:03:45 +0200

    exploring options for filling in missing geometries to SHDI.

--- R/data_shdi.R
+++ R/data_shdi.R
@@ -7,17 +7,56 @@
 #' @param variable one of c("msch", "esch", "lifexp", "lgnic", "shdi")
 #' @export
 gen_shdi <- function(input_folder, variable = "shdi"){
-  shdi <- read.csv(file.path(input_folder, "shdi", "data", "SHDI-Complete SD1.csv"))
-  geom <- sf::read_sf(file.path(input_folder, "shdi", "data", "GDL-SHDI-SHP-2.shp"))
 
-  shdi <- shdi %>%
-    dplyr::filter(level == "Subnat") %>%
-    dplyr::select(year, GDLCODE, !!variable)
+  shape_zip <- file.path(input_folder, "shdi", "data", "GDL Shapefiles V4.zip")
+
+  tdir <- tempdir()
+  unzip(shape_zip, exdir = tdir)
+  shp_fname <- list.files(tdir, pattern = "*.shp")
+  shapefile <- file.path(tdir, shp_fname)
+  geom <- sf::read_sf(shapefile)
+  unlink(tdir)
 
   geom <- geom %>%
-    dplyr::select(GDLCode, geometry)
+    dplyr::rename_all(tolower) %>%
+    dplyr::select(gdlcode, geometry)
 
-  shdi <- merge(geom, shdi, by.x = "GDLCode", by.y = "GDLCODE")
+  old_geom_file <- file.path(input_folder, "shdi", "data", "GDL-SHDI-SHP-2.shp")
+  old_geom <- sf::read_sf(old_geom_file) %>% dplyr::rename_all(tolower)
+
+  shdi <- read.csv(file.path(input_folder, "shdi", "data", "SHDI Complete 4.0 (1).csv"))
+
+  shdi <- shdi %>%
+    dplyr::rename_all(tolower) %>%
+    dplyr::filter(level == "Subnat") %>%
+    dplyr::select(year, gdlcode, !!variable)
+
+  shdi <- dplyr::left_join(shdi, geom, by = "gdlcode") %>% sf::st_as_sf()
+
+  shdi$empty <- sf::st_is_empty(shdi)
+  shdi_empty <- shdi %>% dplyr::filter(empty)
+  sf::st_geometry(shdi_empty) <- NULL
+  shdi_empty %>% dplyr::select(country, region, gdlcode) %>% dplyr::distinct()
+  missing_geometries <- shdi_empty %>% dplyr::select(country, region, gdlcode) %>% dplyr::distinct()
+  missing_in_old <- old_geom %>% dplyr::filter(gdlcode %in% missing_geometries$gdlcode) %>% dplyr::select(gdlcode, geometry)
+  shdi_empty <- dplyr::left_join(shdi_empty, missing_in_old, by = "gdlcode")
+
+
+  shdi <- shdi %>% dplyr::filter(!empty)
+
+  cshp <- cshapes::cshp(date = as.Date("2015-01-01")) %>% sf::st_as_sf()
+  twn <- cshapes::cshp(date = as.Date("2015-01-01")) %>% sf::st_as_sf() %>% dplyr::filter(CNTRY_NAME == "Taiwan")
+  twn$gdlcode <- "CHNr133"
+  twn <- twn %>% dplyr::select(gdlcode, geometry)
+
+  df <- shdi
+  time_fact <- factor(df[[timevar]])
+
+  sdf <- dplyr::select(df, !!variable)
+  sdf_list <- base::split(sdf, time_fact, sep = "_")
+
+  raster::rasterize(as(sdf_list[[1]], "Spatial"))
+  as(polys, "Spatial")
 
   shdi <- priogrid::panel_to_pg(shdi, timevar = "year", variable = variable, need_aggregation = TRUE, missval = -1, fun = "mean")
   return(shdi)
