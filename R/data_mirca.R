b69b4d31343de3e79a1ecf53e3a4f3d7235ca238
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2020-06-30 15:32:44 +0200

    Rewrote mirca gen_*()
    input_folder as first argument; funs now returns PG df rather than raster. Removed unused helper function.

--- R/data_mirca.R
+++ R/data_mirca.R
@@ -1,185 +1,152 @@
+#' @title grow_start
+#'
+#' @description Provides the starting month of the growing season in the cell, based on the Cropping Periods List data
+#' from the MIRCA2000 dataset v.1.1. Snapshot for the year 2000 only.
+#' Values range from 1-12. Defined as the first of five consecutive months with the highest harvested area.
+#'
+#' Link to original data: https://www.uni-frankfurt.de/45218031
+#'
+#' Please cite: Portmann, F. T., Siebert, S. & Döll, P. (2010). MIRCA2000 – Global monthly irrigated and rainfed crop areas around the year 2000: A new high-resolution data set for agricultural and hydrological modeling, Global Biogeochemical Cycles, 24, GB 1011, doi:10.1029/2008GB003435.
+#'
+#' @param input_folder path to [pg-folder].
+#' @param no_months width of rolling window (number of months) used to compute max sum of harvested area. Defaults to 5.
+#' @param variable one of c("grow_start", "grow_end")
+#'
+#' @return a dataframe with x, y, pgid, and grow_start month.
+#'
+#' @export
 
-#' Generate monthly growseas variable
-#' 
-#' Function to generate the area proportion of each cell
-#' that is being harvested for each month. 
-#' 
-#' @param mirca MIRCA2000 data (.TXT)
+gen_growstart <- function(input_folder, variable = "grow_start", no_months = 5){
+  mirca <- prep_growseas(input_folder)
 
-
-gen_growseas <- function(mirca){
-  m_small <- priogrid::prep_growseas(mirca)
-
-  m_raster <- raster::rasterFromXYZ(m_small[c("long", "lat", "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug",
-                                              "sep", "oct", "nov", "dec")], crs = priogrid::prio_crs(), digits = 1)
-
-  agg <- priogrid::prio_aggregate_raster(m_raster, fun = mean)
-  raster::extent(agg) <- priogrid::prio_extent()
-
-  names(agg) <- tolower(month.abb)
-
-  agg
-}
-
-
-
-#' Generate grow_start and grow_end variables
-#' 
-#' Function to generate grow_start and grow_end 
-#' based on MIRCA2000 data.
-#' 
-#' @param mirca MIRCA2000 data (.TXT).
-#' @param no_months Width of rolling window (number of months) used to compute maximum sum of harvested area. Defaults to 5.
-#' 
-#' @return PRIO-GRID-consistend RasterBrick with one layer for each variable.
-
-gen_grow_start_end <- function(mirca, no_months = 5){
-  mirca_prep <- priogrid::prep_growseas(mirca) # NOTE: Does not work - need to figure out data.table dependencies
-  
-  mirca_prep <- mirca_prep %>%
+  mirca <- mirca %>%
     dplyr::mutate(jan2 = jan, feb2 = feb, mar2 = mar, apr2 = apr, may2 = may) %>%
-    tidyr::gather(key = "month", value = "area", -long, -lat) %>%
-    dplyr::mutate(nummonth = match(tolower(month), tolower(month.abb))) %>%
-    dplyr::mutate(nummonth = ifelse(month == "jan2", 13,
-                                    ifelse(month == "feb2", 14,
-                                           ifelse(month == "mar2", 15,
-                                                  ifelse(month == "apr2", 16,
-                                                         ifelse(month == "may2", 17, nummonth)
-                                                  ))))) %>%
+    tidyr::pivot_longer(cols = c(-long, -lat), names_to = "month", values_to = "area") %>%
+    dplyr::group_by(long, lat) %>%
+    dplyr::mutate(nummonth = rep(1:17)) %>%
     dplyr::arrange(long, lat, nummonth) %>%
-    dplyr::mutate(id = dplyr::group_indices())
-  
-  
-  data.table::setDT(mirca_prep)
-  
-  mirca_prep[,consec_mo := zoo::rollapply(area, width = no_months, sum, align = "right", fill = NA, partial = F), by = id]
-  
-  recode_months <- function(var){
-    ifelse(var == 13, 1,
-           ifelse(var == 14, 2,
-                  ifelse(var == 15, 3,
-                         ifelse(var == 16, 4,
-                                ifelse(var == 17, 5,
-                                       var)))))
-  }
-  
-  
-  mirca_final <- mirca_prep %>%
+    dplyr::mutate(id = dplyr::group_indices()) %>%
+    dplyr::ungroup()
+
+  data.table::setDT(mirca)
+  mirca[,consec_mo := zoo::rollapply(area, width = no_months, sum, align = "right", fill = NA, partial = F), by = id]
+
+  mirca <- mirca %>%
     dplyr::mutate(grow_start = ifelse(!is.na(consec_mo), nummonth - (no_months - 1), NA),
                   grow_end = ifelse(!is.na(consec_mo), nummonth, NA)) %>%
-    dplyr::mutate(grow_start = recode_months(grow_start),
-                  grow_end = recode_months(grow_end)) %>%
-    dplyr::group_by(lat, long) %>%
-    dplyr::slice(which.max(consec_mo)) ### NOTE: This approach does NOT consider ties - it chooses the FIRST max for each point by default
-  
-  
-  growseas <- raster::rasterFromXYZ(mirca_final[c("long", "lat", "grow_start", "grow_end")], crs = priogrid::prio_crs(), digits = 1)
-  grow_start <- priogrid::prio_aggregate_raster(growseas[[1]], fun = first)
-  grow_end <- priogrid::prio_aggregate_raster(growseas[[2]], fun = last)
-  growseas <- raster::brick(grow_start, grow_end)
-  names(growseas) <- c("grow_start", "grow_end")
-  
-  growseas
-  
+    dplyr::mutate(grow_start = ifelse(grow_start > 12, grow_start-12, grow_start),
+                  grow_end = ifelse(grow_end > 12, grow_end-12, grow_end)) %>%
+    dplyr::group_by(id) %>%
+    dplyr::slice(which.max(consec_mo)) ### Does NOT consider ties - it chooses the FIRST max for each point by default
+
+
+  grow_start <- raster::rasterFromXYZ(mirca[c("long", "lat", variable)], crs = priogrid::prio_crs(), digits = 5)
+  grow_start <- priogrid::raster_to_pg(grow_start)
+  grow_start <- priogrid::raster_to_tibble(gs, add_pg_index = TRUE)
+  return(grow_start)
 }
 
 
+#' grow_end
+#'
+#' Provides the final month of the growing season in the cell, based on the Cropping Periods List data
+#' from the MIRCA2000 dataset v.1.1. Snapshot for the year 2000 only.
+#' Values range from 1-12. Defined as the last of five consecutive months with the highest harvested area.
+#'
+#' Link to original data: https://www.uni-frankfurt.de/45218031
+#'
+#' Please cite: Portmann, F. T., Siebert, S. & Döll, P. (2010). MIRCA2000 – Global monthly irrigated and rainfed crop areas around the year 2000: A new high-resolution data set for agricultural and hydrological modeling, Global Biogeochemical Cycles, 24, GB 1011, doi:10.1029/2008GB003435.
+#'
+#' @param input_folder path to [pg-folder].
+#' @param no_months width of rolling window (number of months) used to compute max sum of harvested area. Defaults to 5.
+#' @param variable one of c("grow_start", "grow_end")
+#'
+#' @return a dataframe with x, y, pgid, and grow_end month.
+#'
+#' @export
 
-#' Data preparation function
+gen_growend <- function(input_folder, no_months = 5){
+  gen_grow_start(input_folder, variable = "grow_end")
+}
 
-prep_growseas <- function(mirca){
-  # mirca <- read.table(gzfile(mirca), header = T, sep = "\t")
+
+#' grow_area
+#'
+#' Monthly variable, providing the area proportion of each cell
+#' that is being harvested each month. Based on the Cropping Periods List data
+#' from the MIRCA2000 dataset v.1.1. Shapshot for the year 2000 only.
+#'
+#' Link to original data: https://www.uni-frankfurt.de/45218031
+#'
+#' Please cite: Portmann, F. T., Siebert, S. & Döll, P. (2010). MIRCA2000 – Global monthly irrigated and rainfed crop areas around the year 2000: A new high-resolution data set for agricultural and hydrological modeling, Global Biogeochemical Cycles, 24, GB 1011, doi:10.1029/2008GB003435.
+#'
+#' @param input_folder path to [pg-folder].
+#' @param month_no if `TRUE`, months are reported as numeric, 1-12.
+#'
+#' @return a dataframe with x, y, pgid, month, and grow_area proportion.
+#'
+#' @export
+gen_growarea <- function(input_folder, month_no = FALSE){
+  mirca <- prep_growseas(input_folder)
+
+  mirca <- suppressWarnings(raster::rasterFromXYZ(mirca[c("long", "lat", "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug",
+                                                          "sep", "oct", "nov", "dec")], crs = priogrid::prio_crs(), digits = 5))
+
+  mirca <- priogrid::raster_to_pg(mirca, aggregation_function = "mean")
+  mirca <- priogrid::raster_to_tibble(mirca, add_pg_index = TRUE)
+
+  mirca <- mirca %>%
+    tidyr::pivot_longer(cols = c(-x, -y, -pgid),
+                        values_to = "grow_area",
+                        names_to = "month")
+
+  if (month_no){
+    mirca <- mirca %>%
+      dplyr::mutate(month = match(tolower(month), tolower(month.abb)))
+  }
+
+  return(mirca)
+}
+
+
+# data preparation
+prep_growseas <- function(input_folder){
+  mirca <- read.table(gzfile(file.path(input_folder, "growing_periods_listed", "data", "CELL_SPECIFIC_CROPPING_CALENDARS_30MN.TXT.gz")),
+                      header = T, sep = "\t")
 
   m_small <- mirca %>%
-    dplyr::group_by(lat, long, start, end) %>%
+    dplyr::group_by(lat, lon, start, end) %>%
     dplyr::summarize(area = sum(area))
 
+  select_months <- function(start, end, drop_perennials=FALSE){
+    if(is.na(start) | is.na(end)){
+      return(rep(NA, 12))
+    } else if(drop_perennials & start==1 & end==12){ # perennials have growing seasons 1:12
+      return(rep(NA, 12))
+    } else {
+      if(start <= end){
+        return(start:end)
+      } else {
+        return(c(start:12, 1:end))
+      }
+    }
+  }
+
   data.table::setDT(m_small)
-  m_small <- m_small[, list(lat = lat, long = long, area = area, start = start, end = end, month = priogrid::select_months(start, end)),
+
+  m_small <- m_small[, list(lat = lat, lon = lon, area = area, start = start, end = end, month = priogrid::select_months(start, end)),
                      list(1:nrow(m_small))][, nrow := NULL][]
 
-  m_small <- m_small[, list(area = sum(area, na.rm = T)), by = c("lat", "long", "month")]
+  m_small <- m_small[, list(area = sum(area, na.rm = T)), by = c("lat", "lon", "month")]
 
 
   m_small <- m_small %>%
-    dplyr::group_by(lat, long) %>%
+    dplyr::group_by(lat, lon) %>%
     dplyr::mutate(season_weight = area / sum(area)) %>%
-    dplyr::select(lat, long, month, season_weight) %>%
-    tidyr::spread(month, season_weight) %>%
+    dplyr::select(lat, long = lon, month, season_weight) %>%
+    tidyr::pivot_wider(names_from = "month",
+                       values_from = "season_weight") %>%
     magrittr::set_names( c("lat", "long", tolower(month.abb)) )
 
-  m_small
+  return(m_small)
 }
-
-
-select_months <- function(start, end, drop_perennials=FALSE){
-  if(is.na(start) | is.na(end)){
-    return(rep(NA, 12))
-  } else if(drop_perennials & start==1 & end==12){ # perennials have growing seasons 1:12
-    return(rep(NA, 12))
-  } else {
-    if(start <= end){
-      return(start:end)
-    } else {
-      return(c(start:12, 1:end))
-    }
-  }
-}
-
-#' Roll over Columns
-#'
-#' Calculates the sum of a rolling window over a range of columns.
-#' Only considers columns not specified in "exclude"
-#'
-#' Assumes that the columns are sorted
-#'
-#' @param data A set of data
-#' @param exclude Columns to exclude 
-#' @param winsize Size of the window 
-#' @param shift Shift the window. Ex. size 3 / shift 1 gives sum FROM col instead of around
-
-rollovercols <- function(data, fun = sum, winsize = 3, shift = 1, 
-                         exclude = c('lat','long'), debug = FALSE){
-   if(winsize %% 2 == 0){
-      stop(glue::glue('Window size must be odd, not {winsize}!'))
-   } 
-   dnames <- names(data)
-   months <- dnames[!dnames %in% exclude]
-
-   winmid <- ceiling(winsize / 2)
-   winmargin <- (winsize - 1) / 2
-   mids <- seq(winmid,length(months) - winmid,1)
-
-   rowindices <- 1:length(months) 
-   winindices <- c(tail(rowindices,winmargin + abs(shift)),
-                      rowindices,
-                      head(rowindices,winmargin + abs(shift)))
-
-   sumdata <- apply(data,1,function(row){
-      row <- row[months]
-      
-      sapply(1:length(row),function(colindex){
-         mnames <- names(row)
-         wincenter  <- colindex + winmargin
-         w <- seq(colindex + shift + abs(shift),
-                  ((winsize + colindex) - 1) + shift + abs(shift))
-         window <- winindices[w]
-         if(debug){
-            print(glue::glue('Names: {glue::glue_collapse(mnames[window],sep = \',\')}'))
-            print(glue::glue('Values: {glue::glue_collapse(row[window],sep = \',\')}'))
-
-            print(glue::glue('Index: {colindex}'))
-            print(glue::glue('Sum: {sum(row[window])}'))
-         }
-         fun(row[window])
-      })
-   })
-   res <- as.data.frame(t(sumdata))
-   names(res) <- months
-   for(val in exclude){
-      res[val] <- data[val]
-   }
-   res
-}
-
-
