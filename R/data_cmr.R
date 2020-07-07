763971d133099237dbf27caee83579ad0dcd3a3c
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2020-07-07 18:06:43 +0200

    Added various documentation and citations. Cleaned up data_nordhaus and data_cmr.

--- R/data_cmr.R
+++ R/data_cmr.R
@@ -1,84 +1,82 @@
-
 #' @title cmr_mean
 #'
-#' @description Generate mean child malnutrition variable.
+#' @description Measures the average prevalence of child malnutrition within each cell. The original pixel value
+#' is the percent of children under the age of 5 that are malnourished. Based on raster data from the SEDAC Global
+#' Poverty Mapping project. The indicator is a snapshot for the year 2000 only.
+#'
+#' Link to original data: https://sedac.ciesin.columbia.edu/data/set/povmap-global-subnational-prevalence-child-malnutrition
+#'
+#' Please cite: Center for International Earth Science Information Network - CIESIN - Columbia University. 2005. Poverty Mapping Project: Global Subnational Prevalence of Child Malnutrition. Palisades, NY: NASA Socioeconomic Data and Applications Center (SEDAC). https://doi.org/10.7927/H4K64G12.
 #'
 #' @param input_folder Path to [pg-folder].
+#' @param fun one of c("mean", "sd", "min", "max")
 #'
 #' @export
+gen_cmr_mean <- function(input_folder, fun = "mean"){
+   malnut <- raster::raster(file.path(input_folder, "cmr", "data", "uw.asc"))
+   malnut <- malnut/10
 
-gen_cmr_mean <- function(input_folder){
-   cmr_mean <- priogrid::gen_cmr(input_folder, fun = "mean")
+   malnut <- priogrid::raster_to_pg(malnut, aggregation_function = fun)
 
-   names(cmr_mean)[3] <- "cmr_mean"
+   malnut <- priogrid::raster_to_tibble(malnut, add_pg_index = TRUE)
 
-   return(cmr_mean)
+   names(malnut)[3] <- paste0("cmr_", as.character(fun))
+
+   return(malnut)
 }
 
 
 #' @title cmr_sd
 #'
-#' @description Generate standard deviation child malnutrition variable.
+#' @description Measures the prevalence of child malnutrition within each cell. The original pixel value
+#' is the percent of children under the age of 5 that are malnourished. This variable gives the standard deviation
+#' of original pixel values within each cell. Based on raster data from the SEDAC Global Poverty Mapping
+#' project. The indicator is a snapshot for the year 2000 only.
+#'
+#' Link to original data: https://sedac.ciesin.columbia.edu/data/set/povmap-global-subnational-prevalence-child-malnutrition
+#'
+#' Please cite: Center for International Earth Science Information Network - CIESIN - Columbia University. 2005. Poverty Mapping Project: Global Subnational Prevalence of Child Malnutrition. Palisades, NY: NASA Socioeconomic Data and Applications Center (SEDAC). https://doi.org/10.7927/H4K64G12.
 #'
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
 gen_cmr_sd <- function(input_folder){
-   cmr_sd <- priogrid::gen_cmr(input_folder, fun = "sd")
-
-   names(cmr_sd)[3] <- "cmr_sd"
-
-   return(cmr_sd)
+   priogrid::gen_cmr(input_folder, fun = "sd")
 }
 
 #' @title cmr_min
 #'
-#' @description Generate minimum child malnutrition variable.
+#' @description Measures the prevalence of child malnutrition within each cell. The original pixel value
+#' is the percent of children under the age of 5 that are malnourished. This variable gives the minimum
+#' of original pixel values within each cell. Based on raster data from the SEDAC Global Poverty Mapping
+#' project. The indicator is a snapshot for the year 2000 only.
+#'
+#' Link to original data: https://sedac.ciesin.columbia.edu/data/set/povmap-global-subnational-prevalence-child-malnutrition
+#'
+#' Please cite: Center for International Earth Science Information Network - CIESIN - Columbia University. 2005. Poverty Mapping Project: Global Subnational Prevalence of Child Malnutrition. Palisades, NY: NASA Socioeconomic Data and Applications Center (SEDAC). https://doi.org/10.7927/H4K64G12.
 #'
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
 gen_cmr_min <- function(input_folder){
-   cmr_min <- priogrid::gen_cmr(input_folder, fun = "min")
-
-   names(cmr_min)[3] <- "cmr_min"
-
-   return(cmr_min)
+   priogrid::gen_cmr(input_folder, fun = "min")
 }
 
 
 #' @title cmr_max
 #'
-#' @description Generate maximum child malnutrition variable.
+#' @description Measures the prevalence of child malnutrition within each cell. The original pixel value
+#' is the percent of children under the age of 5 that are malnourished. This variable gives the maximum
+#' of original pixel values within each cell. Based on raster data from the SEDAC Global Poverty Mapping
+#' project. The indicator is a snapshot for the year 2000 only.
+#'
+#' Link to original data: https://sedac.ciesin.columbia.edu/data/set/povmap-global-subnational-prevalence-child-malnutrition
+#'
+#' Please cite: Center for International Earth Science Information Network - CIESIN - Columbia University. 2005. Poverty Mapping Project: Global Subnational Prevalence of Child Malnutrition. Palisades, NY: NASA Socioeconomic Data and Applications Center (SEDAC). https://doi.org/10.7927/H4K64G12.
 #'
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
 gen_cmr_max <- function(input_folder){
-   cmr_max <- priogrid::gen_cmr(input_folder, fun = "max")
-
-   names(cmr_max)[3] <- "cmr_max"
-
-   return(cmr_max)
+   priogrid::gen_cmr(input_folder, fun = "max")
 }
-
-
-# Prep function
-gen_cmr <- function(input_folder, fun){
-   malnut <- raster::raster(file_path(input_folder, "cmr", "data", "uw.asc"))
-   malnut <- malnut/10
-
-   raster::extent(malnut) <- priogrid::prio_extent()
-
-   malnut <- priogrid::raster_to_pg(malnut, aggregation_function = fun)
-
-   malnut <- priogrid::rasterextent_to_pg(malnut)
-
-   malnut <- priogrid::raster_to_tibble(malnut, add_pg_index = TRUE)
-
-   return(malnut)
-}
-
