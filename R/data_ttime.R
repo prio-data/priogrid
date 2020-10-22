ea8a2996a9a5f733cdd8e10f40e5854d409dbaab
Vilde Bergstad Larsen <52156207+vildebl@users.noreply.github.com>
2020-10-22 14:44:35 +0200

    Removed rasterextent_to_pg()

--- R/data_ttime.R
+++ R/data_ttime.R
@@ -8,8 +8,6 @@
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
-
 gen_ttime_mean <- function(input_folder){
   ttime_mean <- priogrid::gen_ttime(input_folder, fun = "mean")
 
@@ -29,7 +27,6 @@ gen_ttime_mean <- function(input_folder){
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
 gen_ttime_sd <- function(input_folder){
   ttime_sd <- priogrid::gen_ttime(input_folder, fun = "sd")
 
@@ -50,7 +47,6 @@ gen_ttime_sd <- function(input_folder){
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
 gen_ttime_min <- function(input_folder){
   ttime_min <- priogrid::gen_ttime(input_folder, fun = "min")
 
@@ -69,7 +65,6 @@ gen_ttime_min <- function(input_folder){
 #' @param input_folder Path to [pg-folder].
 #'
 #' @export
-
 gen_ttime_max <- function(input_folder){
   ttime_max <- priogrid::gen_ttime(input_folder, fun = "max")
 
@@ -80,16 +75,11 @@ gen_ttime_max <- function(input_folder){
 
 
 # Preparation function
-
+#' Tidy travel time data
+#' @export
 gen_ttime <- function(input_folder, fun){
   ttime <- raster::raster(file.path(input_folder, "ttime", "data", "2015_accessibility_to_cities_v1.0.tif"))
-
-  raster::extent(ttime) <- priogrid::prio_extent()
-
   ttime <- priogrid::raster_to_pg(ttime, aggregation_function = fun)
-
-  ttime <- priogrid::rasterextent_to_pg(ttime)
-
   ttime <- priogrid::raster_to_tibble(ttime, add_pg_index = TRUE)
 
   return(ttime)
