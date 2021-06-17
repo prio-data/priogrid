ac7f76558ff6c8677ce4ed331dbeaf42f4d9a596
Jonas Vestby <jonves@prio.org>
2021-06-17 09:28:51 +0200

    Adding caveats to setting up the folder structure.

--- R/build_datafolder.R
+++ R/build_datafolder.R
@@ -10,7 +10,27 @@ add_folders <- function(basepath, foldername){
   }
 }
 
-add_datasource <- function(basepath, datasource){
+add_datasource <- function(basepath, datasource, add_large){
+
+  # Not add source if large and flag is FALSE
+  if(add_large == FALSE){
+    if(!is.null(datasource$large)){
+      if(datasource$large == TRUE){
+        return("Data too large for download. Turn add_large = TRUE if you want to download.")
+      }
+    }
+  }
+
+  # No link present, probably need login.
+  if(is.null(datasource$link)|datasource$login == TRUE){
+    return("No link to data or login required.")
+  }
+
+  # Need to add support for this.
+  if(length(datasource$link > 1)){
+    return("More than one data link.")
+  }
+
   # create new folder
   add_folder(basepath, datasource$name)
 
@@ -24,12 +44,18 @@ add_datasource <- function(basepath, datasource){
     download.file(url = datasource$link, destfile = full_file_path)
   }
 
-  file_types_to_extract <- stringr::str_split(datasource$filefilter, "\\|")[[1]] %>% gsub("\\(|\\)", "", x=.)
-  file_types_to_extract <- gsub("\\(|\\)", "", x=datasource$filefilter)
 
   if(tools::file_ext(filename) == "zip"){
+
+    file_types_to_extract <- stringr::str_split(datasource$filefilter, "\\|")[[1]] %>% gsub("\\(|\\)", "", x=.)
+    file_types_to_extract <- gsub("\\(|\\)", "", x=datasource$filefilter)
+
+
     # read zip file and extract files to data folder
-    zip_files_to_extract <- unzip(full_file_path, list = TRUE) %>% filter(grepl(file_types_to_extract, Name)) %>% pull(Name)
+    zip_files_to_extract <- unzip(full_file_path, list = TRUE)
+
+    # user-designated filter
+    #zip_files_to_extract  <- zip_files_to_extract %>% filter(grepl(file_types_to_extract, Name)) %>% pull(Name)
 
     # filter away existing files
     zip_files_to_extract <- zip_files_to_extract[!(zip_files_to_extract %in% files_in_folder)]
@@ -47,7 +73,7 @@ test <- yaml.load_file("~/test.yaml")
 
 
 for(datasource in test$sites){
-  add_datasource(BASE_PATH, datasource)
+  add_datasource(BASE_PATH, datasource, add_large = FALSE)
 }
 
 
