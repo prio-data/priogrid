4ed0d77f48644d670c3d0190139683938a0ee6ba
Jonas Vestby <jonves@prio.org>
2021-06-17 11:43:10 +0200

    added support for IHME GBD stunting data

--- R/data_gbd.R
+++ R/data_gbd.R
@@ -9,18 +9,45 @@
 #' Please cite: Institute for Health Metrics and Evaluation (IHME). Low- and Middle-Income Country Child Growth Failure Geospatial Estimates 2000-2017. Seattle, United States of America: Institute for Health Metrics and Evaluation (IHME), 2020.
 #'
 #' @param input_folder path to [pg-folder].
+#' @param wasting if `TRUE`, calculate wasting data, else calculate stunting data.
 #'
 #' @export
-gen_gbd_wasting <- function(input_folder){
+gen_gbd_wasting <- function(input_folder, wasting = TRUE){
   gbd_files <- list.files(file.path(input_folder, "gbd", "data"), full.names = T)
 
-  wasting_files <- gbd_files[grepl("WASTING", gbd_files)]
+  if(wasting){
+    gbd_files <- gbd_files[grepl("WASTING", gbd_files)]
+  } else {
+    gbd_files <- gbd_files[grepl("STUNTING", gbd_files)]
+  }
 
-  wasting <- raster::brick(lapply(wasting_files, raster::raster))
-  names(wasting) <- paste("year_", sapply(stringr::str_split(names(wasting), "_"), function(x) x[12]), sep = "")
+  r <- raster::brick(lapply(gbd_files, raster::raster))
+  names(r) <- paste("year_", sapply(stringr::str_split(names(r), "_"), function(x) x[12]), sep = "")
 
-  wasting <- priogrid::raster_to_pg(wasting, aggregation_function = "mean")
-  wasting <- priogrid::raster_to_tibble(wasting, add_pg_index = TRUE)
-  wasting <- wasting %>% tidyr::pivot_longer(cols = starts_with("year_"), names_prefix = "year_", names_to = "year", values_to = "wasting")
-  return(wasting)
+  r <- priogrid::raster_to_pg(r, aggregation_function = "mean")
+  df <- priogrid::raster_to_tibble(r, add_pg_index = TRUE)
+
+  if(wasting){
+    df <- df %>% tidyr::pivot_longer(cols = starts_with("year_"), names_prefix = "year_", names_to = "year", values_to = "wasting")
+  } else{
+    df <- df %>% tidyr::pivot_longer(cols = starts_with("year_"), names_prefix = "year_", names_to = "year", values_to = "stunting")
+  }
+  return(df)
+}
+
+#' @title gbd_stunting
+#'
+#' @description Generate mean stunting
+#' for years 2000 - 2017 using data from
+#' the IHME Low- and Middle-Income Country Child Growth Failure Geospatial Estimates 2000-2017.
+#'
+#' Link to original data: http://ghdx.healthdata.org/record/ihme-data/lmic-child-growth-failure-geospatial-estimates-2000-2017.
+#'
+#' Please cite: Institute for Health Metrics and Evaluation (IHME). Low- and Middle-Income Country Child Growth Failure Geospatial Estimates 2000-2017. Seattle, United States of America: Institute for Health Metrics and Evaluation (IHME), 2020.
+#'
+#' @param input_folder path to [pg-folder].
+#'
+#' @export
+gen_gbd_stunting <- function(input_folder, wasting = FALSE){
+  priogrid::gen_gbd_wasting(input_folder, wasting)
 }
