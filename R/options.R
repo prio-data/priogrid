46286c1165984c8651278aa3785e6b724b013fff
Jonas Vestby <jonves@prio.org>
2024-09-06 15:23:33 +0200

    options documentation

--- R/options.R
+++ R/options.R
@@ -1,62 +1,80 @@
 pgconfig_cache <- cachem::cache_disk(dir = rappdirs::user_config_dir("R-priogrid", "prio"))
 
 
-#' PRIO-GRID Options Manager
+#' R6 Class to set and get package-wide options for PRIO-GRID.
 #'
-#' set_extent c(xmin, xmax, ymin, ymax)
-#' set_crs a crs string
-#' set_nrow integer number of rows in output raster
-#' set_ncol integer number of columns in output raster
-#' set_rawfolder the folder where rawdata should be downloaded (possibly very large files), defaults to temporary folder
+#' @description
+#' Options are stored in a persistent file in AppData, see pgconfig_cache$info()
 #'
 #' @examples
 #' pgoptions <- PGOptionsManager$new()
 #' pgoptions$set_rawfolder <- tempfolder()
 #' pgoptions$set_verbose(FALSE)
+#' pgoptions$print()
 PGOptionsManager <- R6::R6Class(
   "PGOptionsManager",
   public = list(
+    #' @description Initialize options
     initialize = function() {
       private$load_options()
     },
 
+    #' @description Set output spatial extent
+    #' @param value A vector with extent, c(xmin, xmax, ymin, ymax)
     set_extent = function(value) {
       private$options$extent <- terra::ext(value)
       private$save_options()
     },
 
+    #' @description Set output CRS
+    #' @param value CRS string
     set_crs = function(value) {
       private$options$crs <- terra::crs(value)
       private$save_options()
     },
 
+    #' @description Set number of rows in output raster
+    #' @param value Integer
     set_nrow = function(value) {
       private$options$nrow <- as.integer(value)
       private$save_options()
     },
 
+    #' @description Set number of cols in output raster
+    #' @param value Integer
     set_ncol = function(value) {
       private$options$ncol <- as.integer(value)
       private$save_options()
     },
 
+    #' @description Set the folder where raw-data should be downloaded (possibly very large files), defaults to temporary folder
+    #' @param value String path to folder
     set_rawfolder = function(value){
       private$options$rawfolder <- normalizePath(value)
       private$save_options()
     },
 
+    #' @description Set verbose output of functions if true.
+    #' @param value Boolean
     set_verbose = function(value){
       private$options$verbose <- value
       private$save_options()
     },
 
+    #' @description Get crs option
     get_crs = function() private$options$crs,
+    #' @description Get extent option
     get_extent = function() private$options$extent,
+    #' @description Get nrow option
     get_nrow = function() private$options$nrow,
+    #' @description Get ncol option
     get_ncol = function() private$options$ncol,
+    #' @description Get rawfolder option
     get_rawfolder = function() private$options$rawfolder,
+    #' @description Get verbose option
     get_verbose = function() private$options$verbose,
 
+    #' @description Prints all options
     print = function() {
       cat("Current options:\n")
       cat("  nrow:", private$options$nrow, "\n")
