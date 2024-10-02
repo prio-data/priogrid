8cf255f1ef6aca746a7e3e16fc421074c7fc867f
Jonas Vestby <jonves@prio.org>
2024-10-02 14:13:55 +0200

    natural earth changed variable names

--- R/data_naturalearth.R
+++ R/data_naturalearth.R
@@ -4,39 +4,39 @@
 #'
 #' @return an object of class sf
 #' @export
-read_naturalearth_10m_physical <- function(){
-  f <- get_pgfile("NaturalEarth10mPhysical", "4.1.1")
+read_naturalearth_50m_land <- function(){
+  f <- get_pgfile("NaturalEarth50mLand", "4.1.1")
   #unzip(f, list = T)
   unzip_to <- file.path(dirname(f), tools::file_path_sans_ext(basename(f)))
   unzip(f, exdir = unzip_to)
   return(sf::st_read(file.path(dirname(f), tools::file_path_sans_ext(basename(f)), "ne_50m_land.shp")))
 }
 
-#' gen_landcover_share
+#' gen_ne_coversh
 #'
 #' Takes the NaturalEarth 10m raster and returns a raster
 #' in PRIO-GRID resolution with the share of the cell that intersects with land.
 #'
 #' @export
-gen_landcover_share <- function(){
-  ne <- read_naturalearth_10m_physical()
+gen_ne_coversh <- function(){
+  ne <- read_naturalearth_50m_land()
 
   pg <- prio_blank_grid()
-  land_cover <- terra::rasterize(terra::vect(ne), pg, fun = "min", cover = T)
-
-  return(land_cover)
+  ne_coversh <- terra::rasterize(terra::vect(ne), pg, fun = "min", cover = T)
+  names(ne_coversh) <- "ne_coversh"
+  return(ne_coversh)
 }
 
-#' gen_pgland
+#' gen_ne_cover
 #'
 #' Takes the NaturalEarth 50m raster and returns
 #' a raster-mask that is true for the grid cells that intersects with land.
 #'
 #' @export
-gen_land <- function(){
-  land_cover_share <- gen_landcover_share()
+gen_ne_cover <- function(){
+  land_cover_share <- gen_ne_coversh()
   pg <- prio_blank_grid()
-  pgland <- terra::intersect(land_cover_share, pg)
-
-  return(pgland)
+  ne_cover <- terra::intersect(land_cover_share, pg)
+  names(ne_cover) <- "ne_cover"
+  return(ne_cover)
 }
