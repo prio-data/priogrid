e4df3068a2fdf0b62f1035271d5a5016e04299cd
Jonas Vestby <jonves@prio.org>
2024-11-20 11:21:19 +0100

    updated download approach, added documentation, fixed variable names for cshapes and naturalearth, written tests

--- R/download_data.R
+++ R/download_data.R
@@ -1,9 +1,28 @@
+#' Download data from url and save to file
+#'
+#' This is a simple wrapper around [httr2::request()] and [httr2::req_perform()].
+#'
+#' @param url The url of the data you want to download.
+#' @param filepath The filepath to where you want to store the data locally.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 download_file_httr2 <- function(url, filepath){
   url |>
     httr2::request() |>
     httr2::req_perform(path = filepath)
 }
 
+#' Extract url- and file-info from PRIO-GRID metadata
+#'
+#' @param use_mirror Boolean. Whether or not to use PRIO-GRID mirror.
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
 pg_rawfiles <- function(use_mirror = TRUE){
   if(use_mirror){
     urls <- pgmeta |>
@@ -19,6 +38,17 @@ pg_rawfiles <- function(use_mirror = TRUE){
   return(urls)
 }
 
+#' Get file-path on local system to a data source in PRIO-GRID
+#'
+#' To look up src_name and version in PRIO-GRID, see [pgmeta] or [pg_rawfiles()].
+#'
+#' @param src_name character, the source name
+#' @param version character, the version number
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_pgfile <- function(src_name, version){
   f <- pg_rawfiles() |> dplyr::filter(src_name == !!rlang::enquo(src_name),
                                       version == !!rlang::enquo(version)) |> dplyr::pull(filename)
@@ -41,7 +71,19 @@ get_pgfile <- function(src_name, version){
   return(full_file_path)
 }
 
-download_pg_rawdata <- function(overwrite = FALSE){
+#' Download the raw-data for PRIO-GRID
+#'
+#' Before running this, you need to set the folder using pgoptions$set_rawfolder("path/to/folder")
+#'
+#' @param overwrite Whether or not to download and overwrite files already in local folder.
+#' @param file_info A data.frame with the same structure as the result from [pg_rawfiles()]. If file_info is null (default),
+#'   then file_info will be all data returned from [pg_rawfiles()].
+#'
+#' @return
+#' @export
+#'
+#' @examples
+download_pg_rawdata <- function(overwrite = FALSE, file_info = NULL){
   destfolder <- pgoptions$get_rawfolder()
 
   if(!dir.exists(destfolder)){
@@ -53,7 +95,12 @@ download_pg_rawdata <- function(overwrite = FALSE){
     )
   }
 
-  file_info <- pg_rawfiles()
+  if(is.null(file_info)){
+    # Default to using all sources.
+    file_info <- pg_rawfiles()
+  }
+
+
   file_info$file_exists <- file.exists(file.path(destfolder, file_info$filename))
   file_info$subdir_exists <- dir.exists(file.path(destfolder, dirname(file_info$filename)))
 
@@ -72,6 +119,14 @@ download_pg_rawdata <- function(overwrite = FALSE){
   }
 
   pb <- utils::txtProgressBar(min = 0, max = total_files, style = 3)
-  mapply(download_file_httr2, url = file_info$url, filepath = file.path(destfolder, file_info$filename))
+  reqs <- mapply(download_file_httr2, url = file_info$url, filepath = file.path(destfolder, file_info$filename), SIMPLIFY = FALSE)
   close(pb)
+
+
+  request_error <- lapply(reqs, httr2::resp_is_error)
+  failed_downloads <- names(reqs)[request_error |> unlist()]
+  if(length(failed_downloads) > 0){
+    print("Failed downloads:")
+    return(failed_downloads)
+  }
 }
