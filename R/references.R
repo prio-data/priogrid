ecb405c53d5991241107a98a4aa2745f86546edd
Jonas Vestby <jonves@prio.org>
2024-11-22 13:29:36 +0100

    read, sort, and print references

---
+++ R/references.R
@@ -0,0 +1,14 @@
+get_bibliography <- function(keys, bib.style = "authoryear", as_biblatex = FALSE){
+  bib <- RefManageR::ReadBib("data/pgmeta.bib")
+  to_cite <- bib[key = keys]
+
+
+  if(as_biblatex){
+    return(RefManageR::toBiblatex(to_cite))
+  } else {
+    print(to_cite, .opts = list("bib.style" = bib.style))
+  }
+}
+
+
+
