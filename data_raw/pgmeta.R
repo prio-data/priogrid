582a8a37acf67595d6f8497433fce00f1ab79b78
krihelskog <krihel@prio.org>
2024-11-26 13:51:16 +0100

    geostat data work in progress

--- data_raw/pgmeta.R
+++ data_raw/pgmeta.R
@@ -31,7 +31,7 @@ cshapes <- dplyr::tibble(
 )
 
 natural_earth <- dplyr::tibble(
-  "src_name" = "NaturalEarth10mLand",
+  "src_name" = "NaturalEarth50mLand",
   "version" = "4.1.1",
   "cite" = "",
   "references" = list(c("")),
@@ -40,11 +40,26 @@ natural_earth <- dplyr::tibble(
   "spatial_extent" = list(c(-180, 180, -90, 90)),
   "crs" = "",
   "temporal_extent" = list(c()),
-  "data_url" = "https://naturalearth.s3.amazonaws.com/4.1.1/10m_physical/ne_10m_land.zip",
+  "data_url" = "https://naturalearth.s3.amazonaws.com/4.1.1/50m_physical/ne_50m_land.zip",
   "prio_mirror_url" = "",
   "status" = "survey"
 )
 
-pgmeta <- dplyr::bind_rows(cshapes, natural_earth)
+geostat <- dplyr::tibble(
+  "src_name" = "GeostatCencusGrid",
+  "version" = "2021",
+  "cite" = "some_bib_key",
+  "references" = list(c("other", "bib", "keys")),
+  "license" = "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+  "spatial_resolution" = "Raster",
+  "spatial_extent" = list(c(-180, 180, -55.90223, 83.11387)),
+  "crs" = "epsg:4326",
+  "temporal_extent" = list(c(1886, 2024)),
+  "data_url" = "https://gisco-services.ec.europa.eu/census/2021/Eurostat_Census-GRID_2021_V2-0.zip",
+  "prio_mirror_url" = "",
+  "status" = "staging"
+)
+
+pgmeta <- dplyr::bind_rows(geostat)
 
 usethis::use_data(pgmeta, overwrite = TRUE)
