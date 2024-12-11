da7e6366e2cc386b6ee4231299f77d22edd78dd5
Jonas Vestby <jonves@prio.org>
2024-12-11 08:51:43 +0100

    rename bibliography for Rdpack

--- R/references.R
+++ R/references.R
@@ -14,7 +14,7 @@
 #' @examples
 #' get_bibliography(keys = "schvitzMappingInternationalSystem2022")
 get_bibliography <- function(keys = NULL, as_biblatex = FALSE){
-  bib <- RefManageR::ReadBib("inst/bibliography.bib")
+  bib <- RefManageR::ReadBib("inst/REFERENCES.bib")
 
   if(is.null(keys)){
     to_cite <- bib
