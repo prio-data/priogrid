270d7faf1d9e829b0698c1228b2384d4529e80f6
Kristine Helskog <krihel@prio.org>
2024-12-17 13:24:19 +0100

    wip

--- R/data_geopko.R
+++ R/data_geopko.R
@@ -1,6 +1,16 @@
 
 ## Geocoded Peacekeeping Operations (Geo-PKO)
 
+read_geopko <- function(){
+  f <- get_pgfile(source_name = "Geocoded Peacekeeping Operations (Geo-PKO)",
+                  source_version = "2.2",
+                  id = "7dcbfbfb-9667-4684-af34-85f69fa8d0a0")
+  df <- readRDS(f)
+  df <- df |>
+    dplyr::filter(!is.na(longitude)) |>
+    sf::st_as_sf(coords = c("longitude", "latitude"),
+                 remove = FALSE) |>
+    sf::st_set_crs(4326)
 
-f <- get_pgfile("Geocoded Peacekeeping Operations (Geo-PKO)", "2.2", "7dcbfbfb-9667-4684-af34-85f69fa8d0a0")
-
+  return(df)
+}
