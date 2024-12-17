f0385447f60c443614a08953833a010855acb817
ingviksm <ingviksm@uio.no>
2024-12-17 13:36:16 +0100

    wip

--- R/mortalityrate.R
+++ R/mortalityrate.R
@@ -1,10 +1,8 @@
-
-
 read_sedac_global_infant_mortality_rates <- function() {
 
   ## URL not avaliable
   #https://sedac.ciesin.columbia.edu/data/set/povmap-global-subnational-infant-mortality-rates-v2-01/data-download
-  fname <- "povmap-global-subnational-infant-mortality-rates-v2-01-geotiff-2/povmap_global_subnational_infant_mortality_rates_v2_01.tif"
+  fname <- "/Users/ingvildsmestad/Desktop/Gitprodjects/raw_data/povmap-global-subnational-infant-mortality-rates-v2-01-geotiff-2/povmap_global_subnational_infant_mortality_rates_v2_01.tif"
   r <- terra::rast(fname)
 
   r <- terra::clamp(r, 0, values = FALSE)
@@ -34,19 +32,4 @@ gen_sedac_global_infant_mortality_rates <- function(data = read_sedac_global_inf
   return(data)
 }
 
-# --- TEST ---
-
-start_read <- Sys.time()
-r_data <- read_sedac_global_infant_mortality_rates()
-time_read <- Sys.time() - start_read
-print(paste("Time to read data:", time_read))
-
-start_gen <- Sys.time()
-r_processed <- gen_sedac_global_infant_mortality_rates(data = r_data)
-time_gen <- Sys.time() - start_gen
-print(paste("Time to process data:", time_gen))
-
-pg <- prio_blank_grid()
-df <- as.data.frame(c(pg, r_processed))
-plot(r_processed)
 
