87e435df9804da4ca87ea01cfed71e3eca42b446
ingviksm <ingviksm@uio.no>
2024-12-17 13:43:35 +0100

    wip

--- R/corruption.R
+++ R/corruption.R
@@ -29,17 +29,17 @@ corruption <- function(data = read_corruption()) {
   return(data)
 }
 
-start_read <- Sys.time()
-r_data <- read_corruption()
-time_read <- Sys.time() - start_read
-print(paste("Time to read data:", time_read))
-
-start_gen <- Sys.time()
-r_processed <- corruption(data = r_data)
-time_gen <- Sys.time() - start_gen
-print(paste("Time to process data:", time_gen))
-
-pg <- prio_blank_grid()
-df <- as.data.frame(c(pg, r_processed))
-plot(r_processed)
+# start_read <- Sys.time()
+# r_data <- read_corruption()
+# time_read <- Sys.time() - start_read
+# print(paste("Time to read data:", time_read))
+#
+# start_gen <- Sys.time()
+# r_processed <- corruption(data = r_data)
+# time_gen <- Sys.time() - start_gen
+# print(paste("Time to process data:", time_gen))
+#
+# pg <- prio_blank_grid()
+# df <- as.data.frame(c(pg, r_processed))
+# plot(r_processed)
 
