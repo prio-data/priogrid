9c4b7c7119d1d94379c13ecf72366f3545b69db8
Kristine Helskog <krihel@prio.org>
2025-01-22 13:31:33 +0100

    WIP

--- R/data_geopko.R
+++ R/data_geopko.R
@@ -5,7 +5,6 @@
 #' @return an object of class sf
 #' @export
 #' \insertRef{cilMappingBlueHelmets2020}{priogrid}
-
 read_geopko <- function(){
   f <- get_pgfile(source_name = "Geocoded Peacekeeping Operations (Geo-PKO)",
                   source_version = "2.2",
@@ -15,30 +14,10 @@ read_geopko <- function(){
     dplyr::filter(!is.na(longitude)) |>
     sf::st_as_sf(coords = c("longitude", "latitude"),
                  remove = FALSE) |>
-    sf::st_set_crs(4326)
+    sf::st_set_crs(4326) |>
+    sf::st_transform(3857)
 
   return(df)
-}
-
-#' Generate Geo-PKO sum
-#'
-#' Takes the number of peacekeeping operations within each PRIO-GRID cell
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' # r <- gen_geopko_sum()
-#' @references
-#' \insertRef{cilMappingBlueHelmets2020}{priogrid}
-
-gen_geopko_sum <- function() {
-  f <- read_geopko()
-  pg <- prio_blank_grid()
-  r <- terra::rasterize(terra::vect(f), pg, field = 1, fun = sum, na.rm = TRUE)
-
-  names(r) <- "geopko_sum"
-
 }
 
 #' Generate Geo-PKO count
@@ -49,26 +28,28 @@ gen_geopko_sum <- function() {
 #' @export
 #'
 #' @examples
-#' # r <- gen_geopko_count()
+#' # r <- gen_geopko_operations_count()
 #' @references
 #' \insertRef{cilMappingBlueHelmets2020}{priogrid}
-
-gen_geopko_count <- function() {
+gen_geopko_operations_count <- function() {
   f <- read_geopko()
   pg <- prio_blank_grid()
 
   years <- 1994:2022
   stack_list <- list()
 
+
   for (year in years) {
+    print(paste("Year:", year))
     c <- f |>
       dplyr::group_by(country, year) |>
-      dplyr::summarise(count = dplyr::n()) |>
-      dplyr::filter(year == !!year)
+      dplyr::summarise(count = dplyr::n())
 
-    r <- terra::rasterize(c, pg, "count")
+    r <- terra::rasterize(c, pg, fun = sum, na.rm = TRUE)
+
 
     stack_list[[as.character(year)]] <- r
+    print(terra::minmax(r))
   }
 
   stack <- terra::rast(stack_list)
