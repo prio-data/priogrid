8704e1a12fb382eec0444adb2804fd0d62814bac
Kristine Helskog <krihel@prio.org>
2025-02-18 09:13:37 +0100

    Read function

--- R/data_geoEPR.R
+++ R/data_geoEPR.R
@@ -2,8 +2,15 @@
 
 read_geoEPR <- function() {
   f <- get_pgfile(source_name = "ETH ICR GeoEPR",
-                  source_version = "2021",
+                  source_version = "2023",
                   id = "3900b527-a728-4c26-b0ab-f4441d3ee2e8")
 
   df <- sf::st_read(f)
+
+  df <- df |>
+    dplyr::mutate(gwsdate = as.Date(paste0(from, "-01-01")),
+           gwedate = as.Date(paste0(to, "-12-31"))) |>
+    dplyr::mutate(date_interval = lubridate::interval(gwsdate, gwedate))
+
+  return(df)
 }
