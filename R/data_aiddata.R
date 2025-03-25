8ece4433bb75cb1f50df5bebbb38c5d005450469
Kristine Helskog <krihel@prio.org>
2025-03-25 11:46:05 +0100

    WIP read func

---
+++ R/data_aiddata.R
@@ -0,0 +1,9 @@
+
+
+read_aiddata <- function() {
+  f <- get_pgfile(source_name = "World Bank Geocoded Research Release",
+                  source_version = "1.4.2",
+                  id = "90f82033-62d2-4751-9b0e-f6008cedb202")
+
+
+}
