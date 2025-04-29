b5085dc6d367ed8555a0c669180c30712923164c
ingviksm <ingviksm@uio.no>
2025-04-29 13:13:21 +0200

    wip

--- R/test_mortalityrate.R
+++ R/test_mortalityrate.R
@@ -1,15 +1,25 @@
-# # --- TEST ---
 
+# Source helper functions (do this only ONCE)
+source("~/Desktop/Gitprodjects/priogrid/R/utility.R")
+source("~/Desktop/Gitprodjects/priogrid/R/options.R")
+
+# --- TEST ---
+
+# Read data
 start_read <- Sys.time()
 r_data <- read_sedac_global_infant_mortality_rates()
 time_read <- Sys.time() - start_read
 print(paste("Time to read data:", time_read))
 
+# Process data
 start_gen <- Sys.time()
 r_processed <- gen_sedac_global_infant_mortality_rates(data = r_data)
 time_gen <- Sys.time() - start_gen
 print(paste("Time to process data:", time_gen))
 
+# Create blank grid and dataframe
 pg <- prio_blank_grid()
 df <- as.data.frame(c(pg, r_processed))
+
+# Plot processed raster
 plot(r_processed)
