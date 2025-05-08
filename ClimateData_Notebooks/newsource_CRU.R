5df0563ed2b8ab488704a61aecd2b14f0bfbb971
gbenz23 <gbenz@terpmail.umd.edu>
2025-05-08 15:19:06 +0200

    wip

---
+++ ClimateData_Notebooks/newsource_CRU.R
@@ -0,0 +1,13 @@
+new_source <- Source$new(
+  source_name = "CRU Climate",
+  source_version = "v4.09",
+  license = "CC BY 4.0",
+  website_url = "https://crudata.uea.ac.uk/cru/data/hrg/",
+  spatial_extent = "World",
+  temporal_resolution = "Monthly",
+  citation_keys = "harrisVersion4CRU2020",  # Semi-colon separated if multiple
+  download_url = "/Users/gbenz/Documents/priogrid/pg_git/ClimateData_Notebooks/CRU_data_ref.txt",
+  tags = "climate, validation"
+)
+
+
