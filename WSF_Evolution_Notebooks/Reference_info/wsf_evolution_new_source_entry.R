31d231eeeea7115941b3d65aab1497e958341a6a
gbenz23 <gbenz@terpmail.umd.edu>
2025-05-13 13:11:34 +0200

    documentation for new_source entry

---
+++ WSF_Evolution_Notebooks/Reference_info/wsf_evolution_new_source_entry.R
@@ -0,0 +1,11 @@
+new_source <- Source$new(
+  source_name = "World Settlement Footprint Evolution",
+  source_version = "updated 2025",
+  license = "CC BY 4.0",
+  website_url = "https://download.geoservice.dlr.de/WSF_EVO/",
+  spatial_extent = "World",
+  temporal_resolution = "Yearly",
+  citation_keys = "marconciniOutliningWhereHumans2020;marconciniUnderstandingCurrentTrends2021",
+  download_url = "/Users/gbenz/Documents/priogrid/pg_git/WSF_Evolution_Notebooks/Reference_info/wsf_evolution_ref.txt",
+  tags = "development"
+)
