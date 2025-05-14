360b12a1e16373fa51ce2714fabebdd452699ff0
Kristine Helskog <krihel@prio.org>
2025-05-14 11:21:30 +0200

    gen percentile distribution of traveltime

--- R/data_traveltime.R
+++ R/data_traveltime.R
@@ -24,29 +24,13 @@ gen_traveltime <- function(percentile = 50) {
     stop("percentile must be one of 25, 50, 75, or 90.")
   }
 
-  travel_time_values <- terra::extract(tt, pg, fun = function(x) {
-    if (length(x) == 0) {
-      return(NA_real_)
-    }
+  quantile_fun <- function(x) {
     return(quantile(x, probs = percentile / 100, na.rm = TRUE))
-  }, na.rm = TRUE)
+  }
 
-  aggregated_raster <- pg
-  values(aggregated_raster) <- travel_time_values
+  aggregated_raster <- terra::aggregate(tt, fact = terra::res(pg)/terra::res(tt), fun = quantile_fun)
 
+  ext(aggregated_raster) <- ext(pg)
   return(aggregated_raster)
-}
-
-
-
-gen_avg_traveltime <- function() {
- tt <- read_traveltime()
- pg <- prio_blank_grid()
- ttagg <- terra::aggregate(tt,
-                          fact = terra::res(pg)/terra::res(tt),
-                          fun = "mean")
-
- names(ttagg) <- "Average travel time to major city"
- return(tt_resamp)
 }
 
