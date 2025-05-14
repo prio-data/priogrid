0246a5b43fc61f0c69de3c75737308b206b6395f
Kristine Helskog <krihel@prio.org>
2025-05-14 15:41:28 +0200

    Documentation

--- R/data_traveltime.R
+++ R/data_traveltime.R
@@ -1,5 +1,12 @@
-
-
+#' Reads the Estimated Travel Time to Major Cities version year 2000 data
+#' Unzips the folder and reads import the .tif file as SpatRaster
+#' Adds date
+#'
+#' @return an object of class SpatRast
+#' @export
+#'
+#' @references
+#' \insertRef{nelsonTravelTimeMajor2008}{priogrid}
 read_traveltime <- function() {
   zip_file <- get_pgfile(source_name = "Estimated Travel Time",
                   source_version = "2000",
@@ -15,7 +22,21 @@ read_traveltime <- function() {
   return(tif_file)
 }
 
-
+#' Generate traveltime
+#'
+#' Aggregates the high resolution travel time data to PRIO-GRID level
+#'
+#' Returns the distribution of travel time for the given percentile.
+#'
+#' @param percentile 25, 50, 75, 90. Default 50.
+#' @export
+#'
+#' @examples
+#' # tt <- gen_traveltime(percentile = 75)
+#'
+#'
+#' @references
+#' \insertRef{nelsonTravelTimeMajor2008}{priogrid}
 gen_traveltime <- function(percentile = 50) {
   tt <- read_traveltime()
   pg <- prio_blank_grid()
