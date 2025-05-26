69891f1767e8c1e437a2c775aaa65e96b87b56a6
Jonas Vestby <jonves@prio.org>
2025-05-26 11:25:48 +0200

    gen_ucdpged_distance_within_country

--- R/data_ucdp.R
+++ R/data_ucdp.R
@@ -20,7 +20,8 @@ ucdpged_distance_within_country <- function(measurement_date, cshp = read_cshape
   pg <- prio_blank_grid()
   ged <- read_ucdp_ged() |>
     sf::st_transform(crs = sf::st_crs(pg)) |>
-    dplyr::filter(lubridate::int_overlaps(date_interval, pg_interval))
+    dplyr::filter(lubridate::int_overlaps(date_interval, pg_interval)) |>
+    dplyr::filter(where_prec < 6) # Remove where_prec >= 6 (i.e. not sub-national precision)
 
   rgw <- cshapes_gwcode(measurement_date, cshp = cshp)
   gwcodes <- unique(cshp$gwcode)
@@ -31,22 +32,31 @@ ucdpged_distance_within_country <- function(measurement_date, cshp = read_cshape
   result <- pg
   result[values(result)] <- 0
   result <- result*cover
-  sf::sf_use_s2(FALSE)
+  suppressMessages(sf::sf_use_s2(FALSE)) # Only use sf here to subset data.
   distances <- list()
   for(gwcode in gwcodes){
     tmp <- rgw
     tmp[values(tmp) != gwcode] <- NA
     tmp[!is.na(values(tmp))] <- 1
 
-    ged_sub <- ged[sf::st_intersects(ged, cshp[cshp$gwcode == gwcode, ], sparse = FALSE),]
+    suppressMessages(
+      ged_sub <- ged[sf::st_intersects(ged, cshp[cshp$gwcode == gwcode, ], sparse = FALSE),]
+    )
+
     if(nrow(ged_sub) > 0){
       dist <- terra::distance(tmp, terra::vect(ged_sub), rasterize = T)
       dist <- (dist*tmp)
       dist[is.na(dist)] <- 0
       result <- result + dist
+    } else{
+      # If no GED events in country, then set the distance to missing (to separate from being 0 m from events)
+      tmp[is.na(values(tmp))] <- 0
+      tmp[values(tmp) == 1] <- NA
+      tmp[values(tmp) == 0] <- 1
+      result <- result * tmp
     }
   }
-  sf::sf_use_s2(TRUE)
+  suppressMessages(sf::sf_use_s2(TRUE))
 
   #plot(1/log1p(result))
   #plot(1/result)
@@ -60,12 +70,37 @@ gen_ucdpged_distance_within_country <- function(ged = read_ucdp_ged(), cshp = re
 
   # Extrapolate cShapes to last UCDP GED date.
   cshp <- cshp |> dplyr::mutate(gweyear = dplyr::if_else(gweyear == max(gweyear), lubridate::year(max(ged$date_end)), gweyear)) |>
-    dplyr::mutate(gwedate = as.Date(paste(gweyear, gwemonth, gweday, sep = "-")))
+    dplyr::mutate(gwedate = as.Date(paste(gweyear, gwemonth, gweday, sep = "-"))) |>
+    dplyr::mutate(date_interval = lubridate::interval(gwsdate, gwedate))
 
+
+  # This does speed up things, but will need to debug.
+  # if(ncores > 1){
+  #   doParallel::registerDoParallel(ncores)
+  #   raster_list <- foreach::foreach(i = 1:length(time_slices)) %dopar% {
+  #     t <- time_slices[i]
+  #     rt <- ucdpged_distance_within_country(t, cshp = cshp)
+  #     names(rt) <- as.character(t)
+  #     return(rt)
+  #   }
+  #   doParallel::stopImplicitCluster()
+  # } else{
+  #   raster_list <- foreach::foreach(i = 1:length(time_slices)) %do% {
+  #     t <- time_slices[i]
+  #     print(t)
+  #     rt <- ucdpged_distance_within_country(t, cshp = cshp)
+  #     names(rt) <- as.character(t)
+  #     return(rt)
+  #   }
+  # }
+  #plot(1/((raster_list[[1]]/1e6)+1))
   r <- ucdpged_distance_within_country(time_slices[1], cshp = cshp)
   for(i in 2:length(time_slices)){
     t <- time_slices[i]
-    terra::add(r) <- ucdpged_distance_within_country(t, cshp = cshp)
+    print(paste("Processing", i,"/",  length(time_slices)))
+    rt <- ucdpged_distance_within_country(t, cshp = cshp)
+    names(rt) <- as.character(t)
+    terra::add(r) <- rt
   }
   names(r) <- as.character(time_slices)
   r
