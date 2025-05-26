b57ed1ca6893ed88dfa679d214aa2108a7812883
gbenz23 <gbenz@terpmail.umd.edu>
2025-05-26 15:37:54 +0200

    ESA references

---
+++ ESA_landcover/References/newsource.R
@@ -0,0 +1,10 @@
+new_source <- Source$new(
+  source_name = "WorldCover 2021",
+  source_version = "v200",
+  license = "CC BY 4.0",
+  website_url = "https://esa-worldcover.org/en/data-access",
+  spatial_extent = "World",
+  temporal_resolution = "Yearly",
+  citation_keys = "zanagaESAWorldCover102022",
+  download_url = "/Users/gbenz/Documents/priogrid/pg_git/ESA_landcover/References/esa_2021_urls.txt",
+  tags = 'landcover' )
