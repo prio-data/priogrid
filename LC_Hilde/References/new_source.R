e0014011f808661ed5d0f929a8fb2ae4e5220e1b
gbenz23 <gbenz@terpmail.umd.edu>
2025-05-26 16:40:50 +0200

    build reference documentation

---
+++ LC_Hilde/References/new_source.R
@@ -0,0 +1,11 @@
+new_source <- Source$new(
+  source_name = "HILDE",
+  source_version = "v1.0",
+  license = "CC BY 4.0",
+  website_url = "https://doi.pangaea.de/10.1594/PANGAEA.921846?format=html#download",
+  spatial_extent = "World",
+  temporal_resolution = "Yearly",
+  citation_keys = "winklerHILDAGlobalLand2020",  # Semi-colon separated if multiple
+  download_url = "https://download.pangaea.de/dataset/921846/files/hildap_vGLOB-1.0_geotiff.zip",
+  tags = "landcover"
+)
