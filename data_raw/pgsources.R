1158ec5c5181e17df8f51016a85f19560130464a
Jonas Vestby <jonves@prio.org>
2025-09-15 13:35:28 +0200

    fix ne source

--- data_raw/pgsources.R
+++ data_raw/pgsources.R
@@ -1,3 +1,7 @@
 pgsources <- readr::read_tsv("data_raw/sources.csv")
 
+if(length(pgsources$id) != length(unique(pgsources$id))){
+  stop("Non-unique IDs. Double check sources.csv")
+}
+
 usethis::use_data(pgsources, overwrite = TRUE)
