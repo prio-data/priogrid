74dd79b00d663ce02e170e4881351453e8eadb69
gbenz23 <gbenz@terpmail.umd.edu>
2025-09-25 11:56:15 +0200

    create GLC folder with relevant files and create new source

---
+++ GLC_Processing/glc__newsource.R
@@ -0,0 +1,11 @@
+new_source <- Source$new( source_name = "GLC_FCS30",
+                          source_version = "v1",
+                          license = "CC BY 4.0",
+                          website_url = "https://zenodo.org/records/8239305",
+                          spatial_extent = "World",
+                          temporal_resolution = "Yearly",
+                          citation_keys = "zhang_glc_fcs30_2021", # Semi-colon separated if multiple
+                          download_url = "/Users/gbenz/Documents/priogrid/pg_git/GLC_Processing/GLC_FC30_downloadlinks.txt",
+                          tags = "landcover")
+
+
