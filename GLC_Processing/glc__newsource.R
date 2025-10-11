f07affa07aa8890d21ae68e531eec5726a52dee7
gbenz23 <gbenz@terpmail.umd.edu>
2025-10-11 12:28:45 +0200

    wip

--- GLC_Processing/glc__newsource.R
+++ GLC_Processing/glc__newsource.R
@@ -4,8 +4,7 @@ new_source <- Source$new( source_name = "GLC_FCS30",
                           website_url = "https://zenodo.org/records/15063683",
                           spatial_extent = "World",
                           temporal_resolution = "Yearly",
-                          citation_keys = "zhang_glc_fcs30_2021", # Semi-colon separated if multiple
+                          citation_keys = "Liu_glc_fcs30_v2_2022", # Semi-colon separated if multiple
                           download_url = "/Users/gbenz/Documents/priogrid/pg_git/GLC_Processing/GLC_FC30_v2_downloadlinks.txt",
                           tags = "landcover")
 
-add_source(new_source)
