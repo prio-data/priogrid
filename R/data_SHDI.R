091a4e4fa3961293446544fe1fd91f617f8d22f5
Kristine Helskog <krihel@prio.org>
2025-11-18 13:34:20 +0100

    started on read function

---
+++ R/data_SHDI.R
@@ -0,0 +1,9 @@
+#' Read the GDL SHDI data
+#'
+#' Reads the downloaded SHDI dataset from GlobalDataLab.
+#'
+#' @return A tibble with SHDI data.
+#' @export
+#'
+#' @references
+#' \insertRef{globaldatalabSubnationalHumanDevelopment2019}{priogrid}
